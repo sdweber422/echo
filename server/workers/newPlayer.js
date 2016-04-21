5131f15111cebf51ac993fb814a3a9cb88717a4d
jeffreywescott <jeffrey@learnersguild.org>
2016-04-20 17:56:36 -0700

    add new players to the GitHub team for their chapter

--- server/workers/newPlayer.js
+++ server/workers/newPlayer.js
@@ -1,7 +1,10 @@
+import fetch from 'isomorphic-fetch'
 import raven from 'raven'
 
 import {getQueue} from '../util'
 import r from '../../db/connect'
+
+const sentry = new raven.Client(process.env.SENTRY_SERVER_DSN)
 
 async function addPlayerToDatabase(user) {
   try {
@@ -20,7 +23,7 @@ async function addPlayerToDatabase(user) {
       createdAt: now,
       updatedAt: now,
     }
-    const savedPlayer = await r.table('players').insert(player).run()
+    const savedPlayer = await r.table('players').insert(player, {returnChanges: 'always'}).run()
     if (savedPlayer.inserted) {
       return savedPlayer.changes[0].new_val
     }
@@ -31,9 +34,28 @@ async function addPlayerToDatabase(user) {
   }
 }
 
-async function addPlayerToGitHubChapterTeam(player) {
+async function addPlayerToGitHubChapterTeam(user, player) {
   try {
-    console.log('saved player:', player)
+    const chapter = await r.table('chapters').get(player.chapterId).run()
+    const fetchOpts = {
+      method: 'PUT',
+      headers: {
+        Authorization: `token ${process.env.GITHUB_ORG_ADMIN_TOKEN}`,
+        Accept: 'application/json',
+      },
+    }
+    console.log(`Adding ${user.handle} to GitHub team ${chapter.channelName} (${chapter.githubTeamId})`)
+    const addToTeamURL = `https://api.github.com/teams/${chapter.githubTeamId}/memberships/${user.handle}`
+    return fetch(addToTeamURL, fetchOpts)
+      .then(resp => {
+        if (!resp.ok) {
+          const respBody = resp.body.read()
+          const errMessage = respBody ? JSON.parse(respBody.toString()) : `FAILED: ${addToTeamURL}`
+          console.error(errMessage)
+          throw new Error(`${errMessage}\n${resp.statusText}`)
+        }
+        return resp.json()
+      })
   } catch (err) {
     console.error(err.stack)
     sentry.captureException(err)
@@ -43,7 +65,7 @@ async function addPlayerToGitHubChapterTeam(player) {
 async function processNewPlayer(user) {
   try {
     const player = await addPlayerToDatabase(user)
-    addPlayerToGitHubChapterTeam(player)
+    await addPlayerToGitHubChapterTeam(user, player)
   } catch (err) {
     console.error(err.stack)
     sentry.captureException(err)
@@ -51,7 +73,6 @@ async function processNewPlayer(user) {
 }
 
 export function start() {
-  const sentry = new raven.Client(process.env.SENTRY_SERVER_DSN)
   const newPlayer = getQueue('newPlayer')
   newPlayer.process(async ({data: user}) => processNewPlayer(user))
 }
