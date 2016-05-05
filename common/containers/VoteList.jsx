4ee37f57a21da54e8ded9a9d3e043815329dd136
jeffreywescott <jeffrey@learnersguild.org>
2016-05-05 16:22:40 -0700

    programmatic mocks

--- common/containers/VoteList.jsx
+++ common/containers/VoteList.jsx
@@ -2,62 +2,49 @@ import {connect} from 'react-redux'
 
 import VoteList from '../components/VoteList'
 
+// TODO BEGIN: this is temporary while we mock-up the UI
+function uuid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+    const r = Math.random() * 16 | 0
+    const v = c === 'x' ? r : (r & 0x3 | 0x8)
+    return v.toString(16)
+  })
+}
+function mockPlayerIds(howMany = Math.floor(Math.random() * 5) + 1) {
+  return Array.from(Array(howMany).keys()).map(() => uuid())
+}
+function mockGoal() {
+  const goalNum = Math.floor(Math.random() * 40) + 5
+  return {
+    url: `https://github.com/GuildCraftsTesting/web-development-js-testing/issues/${goalNum}`,
+    name: `goal #${goalNum}`,
+  }
+}
+function mockVote() {
+  return {
+    id: uuid(),
+    playerIds: mockPlayerIds(),
+    goal: mockGoal(),
+  }
+}
+function mockVotes(howMany) {
+  return Array.from(Array(howMany).keys()).map(() => mockVote())
+}
+// TODO END: this is temporary while we mock-up the UI
+
 function mapStateToProps(/* state */) {
   const chapter = {
-    id: '2155c6c4-16c6-4def-9d09-46002cf00e9e',
+    id: uuid(),
     name: 'Oakland',
     goalRepositoryURL: 'https://github.com/GuildCraftsTesting/web-development-js-testing',
   }
   const cycle = {
-    id: '3155c6c4-16c6-4def-9d09-46002cf00e9e',
+    id: uuid(),
     cycleNumber: 2,
     startTimestamp: new Date(),
     state: 'GOAL_SELECTION',
   }
-  const votes = [{
-    id: '4155c6c4-16c6-4def-9d09-46002cf00e9e',
-    playerIds: [
-      '4155c6c4-16c6-4def-9d09-46002cf01e9e',
-      '4155c6c4-16c6-4def-9d09-46002cf02e9e',
-      '4155c6c4-16c6-4def-9d09-46002cf03e9e',
-      '4155c6c4-16c6-4def-9d09-46002cf04e9e',
-      '4155c6c4-16c6-4def-9d09-46002cf05e9e',
-    ],
-    goal: {
-      url: 'https://github.com/GuildCraftsTesting/web-development-js-testing/issues/45',
-      name: 'rerum placeat accusantium',
-    },
-  }, {
-    id: '5155c6c4-16c6-4def-9d09-46002cf00e9e',
-    playerIds: [
-      '4155c6c4-16c6-4def-9d09-46002cf31e9e',
-      '4155c6c4-16c6-4def-9d09-46002cf32e9e',
-      '4155c6c4-16c6-4def-9d09-46002cf33e9e',
-    ],
-    goal: {
-      url: 'https://github.com/GuildCraftsTesting/web-development-js-testing/issues/46',
-      name: 'et blanditiis illum',
-    },
-  }, {
-    id: '6155c6c4-16c6-4def-9d09-46002cf00e9e',
-    playerIds: [
-      '4155c6c4-16c6-4def-9d09-46002cf11e9e',
-      '4155c6c4-16c6-4def-9d09-46002cf12e9e',
-    ],
-    goal: {
-      url: 'https://github.com/GuildCraftsTesting/web-development-js-testing/issues/47',
-      name: 'ratione eum ipsum',
-    },
-  }, {
-    id: '7155c6c4-16c6-4def-9d09-46002cf00e9e',
-    playerIds: [
-      '4155c6c4-16c6-4def-9d09-46002cf21e9e',
-    ],
-    goal: {
-      url: 'https://github.com/GuildCraftsTesting/web-development-js-testing/issues/48',
-      name: 'repudiandae labore et',
-    },
-  }]
+  const votes = mockVotes(50).sort((voteA, voteB) => voteB.playerIds.length - voteA.playerIds.length)
 
   return {
     chapter,
