74fd661a36cf56c41682e85400cb55e08548a8f3
jeffreywescott <jeffrey@learnersguild.org>
2016-05-05 20:37:22 -0700

    tests for the CandidateGoal component

---
+++ common/components/__tests__/CandidateGoal-test.js
@@ -0,0 +1,78 @@
+import test from 'ava'
+import faker from 'faker'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TestUtils from 'react-addons-test-utils'
+
+import CandidateGoal from '../CandidateGoal'
+import styles from '../CandidateGoal.css'
+import factory from '../../../test/factories'
+
+const mockCandidateGoal = {
+  id: faker.random.uuid(),
+  playerIds: Array.from(Array(3).keys()).map(() => faker.random.uuid()),
+  goal: {
+    url: 'https://github.com/GuildCraftsTesting/web-development-js-testing/issues/40',
+    name: 'the goal name (#40)',
+  },
+}
+
+test('renders the goal name', t => {
+  t.plan(1)
+
+  const root = TestUtils.renderIntoDocument(
+    React.createElement(CandidateGoal, {
+      currentUser: factory.build('player'),
+      candidateGoal: mockCandidateGoal,
+    })
+  )
+  const rootNode = ReactDOM.findDOMNode(root)
+
+  t.true(rootNode.textContent.indexOf(mockCandidateGoal.goal.name) >= 0)
+})
+
+test('renders the number of votes', t => {
+  t.plan(1)
+
+  const root = TestUtils.renderIntoDocument(
+    React.createElement(CandidateGoal, {
+      currentUser: factory.build('player'),
+      candidateGoal: mockCandidateGoal,
+    })
+  )
+  const rootNode = ReactDOM.findDOMNode(root)
+
+  t.true(rootNode.textContent.indexOf(`${mockCandidateGoal.playerIds.length}`) >= 0)
+})
+
+test('renders a link to the goal', t => {
+  t.plan(1)
+
+  const root = TestUtils.renderIntoDocument(
+    React.createElement(CandidateGoal, {
+      currentUser: factory.build('player'),
+      candidateGoal: mockCandidateGoal,
+    })
+  )
+  const link = TestUtils.findRenderedDOMComponentWithTag(root, 'a')
+
+  t.is(link.href, mockCandidateGoal.goal.url)
+})
+
+test('provides an indication that the current player voted for the given goal', t => {
+  t.plan(1)
+
+  const currentUser = factory.build('player')
+  const mcg = Object.assign({}, mockCandidateGoal)
+  mcg.playerIds[0] = currentUser.id
+  const root = TestUtils.renderIntoDocument(
+    React.createElement(CandidateGoal, {
+      currentUser,
+      candidateGoal: mcg,
+    })
+  )
+  const votedEls = TestUtils.scryRenderedDOMComponentsWithClass(root, styles.voted)
+
+  t.true(votedEls.length > 0)
+})
