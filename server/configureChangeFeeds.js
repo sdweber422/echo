b9c516e51b44e069da6d2ec89b08deffe24af77a
jeffreywescott <jeffrey@learnersguild.org>
2016-05-10 09:41:02 -0700

    log errors to sentry

--- server/configureChangeFeeds.js
+++ server/configureChangeFeeds.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-console, camelcase */
+import raven from 'raven'
+
 import r from '../db/connect'
 import {getQueue} from './util'
+
+const sentry = new raven.Client(process.env.SENTRY_SERVER_DSN)
 
 function newChapters() {
   const newChapterQueue = getQueue('newChapter')
@@ -8,10 +12,13 @@ function newChapters() {
   r.table('chapters').changes().filter(r.row('old_val').eq(null))
     .then(cursor => {
       cursor.each((err, {new_val: chapter}) => {
-        if (!err) {
-          console.log('adding new chapter to newChapter queue:', chapter)
-          newChapterQueue.add(chapter)
+        if (err) {
+          console.error(err)
+          sentry.captureException(err)
+          return
         }
+        console.log('adding new chapter to newChapter queue:', chapter)
+        newChapterQueue.add(chapter)
       })
     })
 }
@@ -24,10 +31,13 @@ function newOrUpdatedVotes() {
     .filter(r.row('new_val')('goals').filter(goal => goal.hasFields(['githubIssue']).not()).count().gt(0))
     .then(cursor => {
       cursor.each((err, {new_val: vote}) => {
-        if (!err) {
-          // console.log('adding vote to vote queue:', vote)
-          newOrUpdatedVoteQueue.add(vote)
+        if (err) {
+          console.error(err)
+          sentry.captureException(err)
+          return
         }
+        // console.log('adding vote to vote queue:', vote)
+        newOrUpdatedVoteQueue.add(vote)
       })
     })
 }
