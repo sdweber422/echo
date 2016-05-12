e2babf3ec79da0cfacb38eae80673d82f9c8f3aa
jeffreywescott <jeffrey@learnersguild.org>
2016-05-12 15:32:06 -0700

    add tests for mergeEntitiets

--- common/util/__tests__/mergeEntities.test.js
+++ common/util/__tests__/mergeEntities.test.js
@@ -1,4 +1,27 @@
-import test from 'ava'
+/* eslint-env mocha */
+/* global expect, testContext */
+/* eslint-disable prefer-arrow-callback, no-unused-expressions */
 
-test.todo('returns the original entities unchanged if there are no updates')
-test.todo('merges each individual object')
+import mergeEntities from '../mergeEntities'
+
+describe(testContext(__filename), function () {
+  before(function () {
+    this.original = {
+      a: 1,
+      b: {
+        c: 3,
+        d: 4,
+      },
+    }
+  })
+
+  it('returns the original entities unchanged if there are no updates', function () {
+    const updated = mergeEntities(this.original)
+    expect(updated).to.deep.equal(this.original)
+  })
+
+  it('merges each individual object', function () {
+    const updated = mergeEntities(this.original, {b: {d: 'four', e: 5}, f: 6})
+    expect(updated).to.deep.equal({a: 1, b: {c: 3, d: 'four', e: 5}, f: 6})
+  })
+})
