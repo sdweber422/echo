e3c0c57c490c0b77a157e7689ae9b5bc9a701e0d
jeffreywescott <jeffrey@learnersguild.org>
2016-05-12 15:31:35 -0700

    helper function to print out which module we are testing

--- config/testConfig.js
+++ config/testConfig.js
@@ -15,6 +15,16 @@ global.document = doc
 global.window = win
 global.navigator = win.navigator
 
+// helpers
+global.testContext = filename => {
+  return filename.slice(1).split('/').reduce((ret, curr) => {
+    const currWithoutTests = curr === '__tests__' ? null : `/${curr}`
+    const value = ret.useCurr && currWithoutTests ? `${ret.value}${currWithoutTests}` : ret.value
+    const useCurr = ret.useCurr || curr === 'game'
+    return {useCurr, value}
+  }, {useCurr: false, value: ''}).value.replace('.test.js', '').slice(1)
+}
+
 // setup chai and make it available in all tests
 chai.use(chaiAsPromised)
 global.expect = chai.expect
