29541ee6565f0cf34a16f3fa1ebf75b7b830fece
Trevor Little <trevor@learnersguild.org>
2016-05-23 00:36:52 -0400

    wip - implemented rand algorithm with correct team sizes

--- server/actions/__tests__/formProjectTeams.test.js
+++ server/actions/__tests__/formProjectTeams.test.js
@@ -1,55 +1,82 @@
 /* eslint-env mocha */
 /* global expect, testContext */
 /* eslint-disable prefer-arrow-callback, no-unused-expressions */
+var util = require('util');
+function debug(x) {
+  console.log(util.inspect(x, {showHidden: false, depth: null}))
+}
 
 import r from '../../../db/connect'
 import factory from '../../../test/factories'
 import {withDBCleanup, pending} from '../../../test/helpers'
 
-import formProjectTeams from '../formProjectTeams'
+import {GOAL_SELECTION} from '../../../common/models/cycle'
+const {formProjectTeams, forTesting: {getTeamSizes}} = require('../formProjectTeams')
 
 describe(testContext(__filename), function () {
   withDBCleanup()
 
-  pending('creates Teams', async function () {
-    try {
-      const cycle = await factory.create('cycle')
-      const players = await factory.createMany('player', 5, {chapterId: cycle.chapterId})
-      const votes = await factory.createMany('vote', 5,
-        [[1,2], [1,2], [2,3], [3,4], [3,4]].map(
-          ([a,b], i) => ({
-            cycleId: cycle.id,
-            playerId: players[i],
-            goals: [
-              {url: `http://ex.co/${a}`, title: `Goal ${a}`},
-              {url: `http://ex.co/${b}`, title: `Goal ${b}`},
-            ]
-          })
+  describe('formProjectTeams', function() {
+    it.only('creates teams based on player votes', async function () {
+      try {
+        const cycle = await factory.create('cycle', {state: GOAL_SELECTION})
+        const players = await factory.createMany('player', {chapterId: cycle.chapterId}, 6)
+        const votes = await factory.createMany('vote',
+          [[1,2], [1,2], [1,3], [3,4], [3,4], [6,7]].map(
+            ([a,b], i) => ({
+              cycleId: cycle.id,
+              playerId: players[i].id,
+              goals: [
+                {url: `http://ex.co/${a}`, title: `Goal ${a}`},
+                {url: `http://ex.co/${b}`, title: `Goal ${b}`},
+              ]
+            })
+          ),
+          6,
         )
-      )
+        debug({votes})
 
-      const result = await formProjectTeams(cycle)
-      const createdProjects = await r.table('projects').run()
-      expect(createdProjects).to.have.length(2)
+        const result = await formProjectTeams(cycle.id)
+        const createdProjects = await r.table('projects').run()
+        console.log({createdProjects})
+        expect(createdProjects).to.have.length(2)
 
-      const goal1Project = createdProjects.filter(p => p.goalUrl.endsWith('1'))
-      expect(goal1Project).to.exist
-      expect(goal1Project.name).to.match(/^\w+-\w+-\d$/)
+        const goal1Project = createdProjects.filter(p => p.goalUrl.endsWith('1'))[0]
+        expect(goal1Project).to.exist
+        expect(goal1Project.name).to.match(/^\w+-\w+(-\d)?$/)
 
-      const goal3Project = createdProjects.filter(p => p.goalUrl.endsWith('3'))
-      expect(goal3Project).to.exist
-      expect(goal3Project.name).to.match(/^\w+-\w+-\d$/)
+        const goal3Project = createdProjects.filter(p => p.goalUrl.endsWith('3'))[0]
+        expect(goal3Project).to.exist
+        expect(goal3Project.name).to.match(/^\w+-\w+(-\d)?$/)
 
-      const goal1ProjectTeam = goal1Project.cycleTeams[cycle.id].sort()
-      expect(goal1ProjectTeam).to.deep.equal(players.slice(0, 2))
+        const goal1ProjectTeam = goal1Project.cycleTeams[cycle.id].playerIds.sort()
+        console.log({goal1ProjectTeam})
+        expect(goal1ProjectTeam).to.deep.equal(players.map(p => p.id).slice(0, 3))
 
-      // The middle player voted for both slots, but should get their first choice
-      const goal3ProjectTeam = goal3Project.cycleTeams[cycle.id].sort()
-      expect(goal3ProjectTeam).to.deep.equal(players.slice(3, 5))
+        // The middle player voted for both slots, but should get their first choice
+        const goal3ProjectTeam = goal3Project.cycleTeams[cycle.id].playerIds.sort()
+        console.log({goal3ProjectTeam})
+        expect(goal3ProjectTeam).to.deep.equal(players.map(p => p.id).slice(4, 6))
 
-    } catch (e) {
-      throw(e)
-    }
+      } catch (e) {
+        throw(e)
+      }
+    })
+    it('players who did not vote are assigned to teams')
+    it('each team has at least one highly skilled player')
   })
-  it('makes sure team has at least one highly skilled player')
+
+  describe('getTeamSizes(playerCount, target)', function() {
+
+    it('determines optimal team sizes based on playerCount', function() {
+      expect(getTeamSizes(6).sort()).to.deep.equal([3,3].sort())
+      expect(getTeamSizes(9).sort()).to.deep.equal([5,4].sort())
+      expect(getTeamSizes(11).sort()).to.deep.equal([4,4,3].sort())
+      expect(getTeamSizes(12).sort()).to.deep.equal([4,4,4].sort())
+      expect(getTeamSizes(13).sort()).to.deep.equal([4,4,5].sort())
+      expect(getTeamSizes(14).sort()).to.deep.equal([4,5,5].sort())
+    })
+
+  })
 })
+
--- server/actions/formProjectTeams.js
+++ server/actions/formProjectTeams.js
@@ -1,2 +1,111 @@
-export default function formProjectTeams() {
+import animal from 'animal-id'
+import {graphql} from 'graphql'
+import r from '../../db/connect'
+import rootSchema from '../graphql/rootSchema'
+import {graphQLErrorHander} from '../../common/util/getGraphQLFetcher'
+
+function getCycleVotingResults(cycleId) {
+  const query = `
+    query($cycleId: ID!) {
+      getCycleVotingResults(cycleId: $cycleId) {
+        id
+        cycle {
+          id
+          chapter {
+            id
+            name
+            channelName
+            goalRepositoryURL
+          }
+        }
+        numEligiblePlayers
+        numVotes
+        candidateGoals {
+          goal {
+            url
+            title
+          }
+          playerGoalRanks {
+            playerId
+            goalRank
+          }
+        }
+      }
+    }
+  `
+
+  return graphql(rootSchema, query, {currentUser: true}, {cycleId})
+    .then(graphQLErrorHander)
+    .then(results => results.data.getCycleVotingResults)
+}
+
+module.exports.forTesting = {}
+module.exports.forTesting = {getTeamSizes}
+function getTeamSizes(playerCount, target=4) {
+  const absoluteMinumum = 3
+  const min = target - 1
+
+  const div = Math.floor(playerCount / target)
+  const remainder = playerCount % target
+
+  if (remainder === 0) {
+    return Array.from({length: div}, () => target)
+  }
+
+  if (remainder >= min) {
+    const perfectTeams = Array.from({length: div}, () => target)
+    return perfectTeams.concat(remainder)
+  }
+
+  if (remainder < min && div >= remainder) {
+    const perfectTeams = Array.from({length: div - remainder}, () => target)
+    const largerTeams = Array.from({length: remainder}, () => target + 1)
+    return perfectTeams.concat(...largerTeams)
+  }
+
+  if (target > absoluteMinumum) {
+    return getTeamSizes(playerCount, target-1)
+  }
+
+  throw Error('I cannot figure what the team sizes should be!')
+}
+
+export async function formProjectTeams(cycleId) {
+  try {
+    const votingResults = await getCycleVotingResults(cycleId)
+    // console.log(votingResults.candidateGoals.map(g => g.playerGoalRanks))
+    const chapterPlayers = await r.table('players').getAll(votingResults.cycle.chapter.id, {index: 'chapterId'}).run()
+
+    // 1. Figure out how many teams there need to be for 3-5 players each
+    const teamSizes = getTeamSizes(chapterPlayers.length)
+
+    // 2. get that many goals (using the ones with the most votes)
+    const candidateGoals = votingResults.candidateGoals.slice(0, teamSizes.length)
+
+    // 3. create 1 project for each goal
+    const now = new Date()
+    return Promise.all(candidateGoals.map((candidateGoal, i) => {
+      const goalUrl = candidateGoal.goal.url
+      const team = chapterPlayers.splice(0, teamSizes[i])
+
+      return r.table('projects').insert({
+        goalUrl: goalUrl,
+        // TODO: add unique index on this name
+        name: animal.getId(),
+        chapterId: votingResults.cycle.chapter.id,
+        cycleTeams: {
+          [cycleId]: { playerIds: team.map(p => p.id) }
+        },
+        createdAt: now,
+        updatedAt: now,
+      }).run()
+    }))
+
+    // TODO:
+    //   * use very popular goals for more than one project
+    //   *
+    //
+  } catch (e) {
+    throw(e)
+  }
 }
