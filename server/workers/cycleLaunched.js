8343495cff18f9af5aad99dfaba3c4567ccef725
Trevor Little <trevor@learnersguild.org>
2016-05-23 14:23:48 -0400

    moving stuff around. also ensuring channels are created before their mentioned

--- server/workers/cycleLaunched.js
+++ server/workers/cycleLaunched.js
@@ -4,19 +4,17 @@ import ChatClient from '../../server/clients/ChatClient'
 import r from '../../db/connect'
 import {formProjectTeams} from '../../server/actions/formProjectTeams'
 
-function getPlayerHandles(playerIds) {
-  return r.table('players').getAll(...playerIds).pluck('handle')
-    .run()
-    .then(rows => rows.map(row => row.handle))
+export function start() {
+  const cycleLaunched = getQueue('cycleLaunched')
+  cycleLaunched.process(({data: cycle}) => processCycleLaunch(cycle))
 }
 
 function processCycleLaunch(cycle) {
   return formProjectTeams(cycle.id)
-    .then(projects => Promise.all(
-      projects.map(project => initializeProjectChannel(project.name, project.cycleTeams[cycle.id].playerIds)).concat(
-        sendCycleLaunchAnnouncement(cycle, projects)
-      )
-    ))
+    .then(projects =>
+      Promise.all(projects.map(project => initializeProjectChannel(project.name, project.cycleTeams[cycle.id].playerIds)))
+        .then(() => sendCycleLaunchAnnouncement(cycle, projects))
+    )
     .catch(e => console.log(e))
 }
 
@@ -27,6 +25,12 @@ function initializeProjectChannel(channelName, playerIds) {
     .then(() => client.sendMessage(channelName, `Welcome to the ${channelName} project channel!`))
 }
 
+function getPlayerHandles(playerIds) {
+  return r.table('players').getAll(...playerIds).pluck('handle')
+    .run()
+    .then(rows => rows.map(row => row.handle))
+}
+
 function sendCycleLaunchAnnouncement(cycle, projects) {
   const projectListString = projects.map(p => `#${p.name}`).join(', ')
   const announcement = `🚀 The cycle been launched and the following projects have been created: ${projectListString}`
@@ -34,9 +38,4 @@ function sendCycleLaunchAnnouncement(cycle, projects) {
 
   return r.table('chapters').get(cycle.chapterId).run()
     .then(chapter => client.sendMessage(chapter.channelName, announcement))
-}
-
-export function start() {
-  const cycleLaunched = getQueue('cycleLaunched')
-  cycleLaunched.process(({data: cycle}) => processCycleLaunch(cycle))
 }
