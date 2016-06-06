6b8cca9169caeb5b38b6da65f6111e8c5caffcc9
Trevor Little <trevor@learnersguild.org>
2016-06-06 16:07:48 -0400

    fixing lint error. removing debugging catch

--- test/helpers/db.js
+++ test/helpers/db.js
@@ -4,7 +4,6 @@ function truncateDBTables() {
   return r.tableList()
     .then(tables => tables.filter(t => !t.startsWith('_')))
     .then(tablesToTruncate => Promise.all(tablesToTruncate.map(t => r.table(t).delete().run())))
-    .catch(e => console.error('truncateDBTables ERROR: ', e))
 }
 
 /* eslint-env mocha */
