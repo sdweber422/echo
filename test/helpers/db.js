bba5710325a597f10f70e142482eb8dfe8feaf11
Trevor Little <trevor@learnersguild.org>
2016-06-06 13:08:14 -0400

    debugging circleCI

--- test/helpers/db.js
+++ test/helpers/db.js
@@ -4,6 +4,7 @@ function truncateDBTables() {
   return r.tableList()
     .then(tables => tables.filter(t => !t.startsWith('_')))
     .then(tablesToTruncate => Promise.all(tablesToTruncate.map(t => r.table(t).delete().run())))
+    .catch(e => console.error('truncateDBTables ERROR: ', e))
 }
 
 /* eslint-env mocha */
