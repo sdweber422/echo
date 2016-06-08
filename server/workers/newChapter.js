7fa81785e7a6e0312ab38df6876988cd1cb5ef62
jeffreywescott <jeffrey@learnersguild.org>
2016-06-08 16:17:32 -0700

    if GitHub team already exists, fetch it
    This should really only happen if a chapter with the same channelName gets created more than once. In reality, this should only happen when running the `npm run data:playtest` script.
    
    We will inspect the response body that comes back from GitHub to see if it is this one particular error code. If it is, we'll call back out to GitHub to fetch the correct team with the same name (so that we can update the githubTeamId on the chapter).

--- server/workers/newChapter.js
+++ server/workers/newChapter.js
@@ -7,6 +7,43 @@ import {getQueue} from '../util'
 
 const sentry = new raven.Client(process.env.SENTRY_SERVER_DSN)
 
+function getGitHubTeam(owner, name) {
+  const fetchOpts = {
+    method: 'GET',
+    headers: {
+      Authorization: `token ${process.env.GITHUB_ORG_ADMIN_TOKEN}`,
+      Accept: 'application/json',
+    },
+  }
+  console.log(`Fetching GitHub team for ${owner}/${name}`)
+  const getTeamsURL = `https://api.github.com/orgs/${owner}/teams`
+  return fetch(getTeamsURL, fetchOpts)
+    .then(resp => {
+      if (!resp.ok) {
+        const respBody = resp.body.read()
+        const ghResponse = respBody && JSON.parse(respBody.toString())
+        const errMessage = ghResponse ? ghResponse : `FAILED: ${getTeamsURL}`
+        console.error(errMessage)
+        throw new Error(`${errMessage}\n${resp.statusText}`)
+      }
+      return resp.json()
+    })
+    .then(teams => {
+      return teams.filter(team => team.name === name)[0]
+    })
+}
+
+const GITHUB_ALREADY_EXISTS_ERROR_CODE = 'already_exists'
+function isTeamAlreadyExistsError(ghResponse) {
+  return (
+    ghResponse &&
+    ghResponse.errors &&
+    ghResponse.errors.reduce((shouldIgnore, currentError) => {
+      return shouldIgnore && (currentError.code === GITHUB_ALREADY_EXISTS_ERROR_CODE)
+    }, true)
+  )
+}
+
 function createGitHubTeamWithAccessToGoalRepo(chapter) {
   const {owner, repo} = getOwnerAndRepoFromGitHubURL(chapter.goalRepositoryURL)
   const body = {
@@ -23,15 +60,20 @@ function createGitHubTeamWithAccessToGoalRepo(chapter) {
     },
     body: JSON.stringify(body),
   }
-  console.log(`Creating GitHub team ${body.name} for chapter ${chapter.id}`)
+  console.log(`Creating GitHub team ${owner}/${body.name}`)
   const createTeamURL = `https://api.github.com/orgs/${owner}/teams`
   return fetch(createTeamURL, fetchOpts)
     .then(resp => {
       if (!resp.ok) {
         const respBody = resp.body.read()
-        const errMessage = respBody ? JSON.parse(respBody.toString()) : `FAILED: ${createTeamURL}`
-        console.error(errMessage)
-        throw new Error(`${errMessage}\n${resp.statusText}`)
+        const ghResponse = respBody && JSON.parse(respBody.toString())
+        if (!isTeamAlreadyExistsError(ghResponse)) {
+          const errMessage = ghResponse ? ghResponse : `FAILED: ${createTeamURL}`
+          console.error(errMessage)
+          throw new Error(`${errMessage}\n${resp.statusText}`)
+        }
+        console.log(`GitHub team ${owner}/${body.name} already exists`)
+        return getGitHubTeam(owner, body.name)
       }
       return resp.json()
     })
@@ -39,6 +81,7 @@ function createGitHubTeamWithAccessToGoalRepo(chapter) {
 
 async function addTeamIdToChapter(chapter, team) {
   try {
+    console.log(`Adding GitHub team id (${team.id}) to chapter (${chapter.id})`)
     const savedChapter = await r.table('chapters')
       .get(chapter.id)
       .update({githubTeamId: team.id}, {returnChanges: 'always'})
