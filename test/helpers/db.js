8c31d49dc1a384e46bd90423a0f0dd90872eb938
jeffreywescott <jeffrey@learnersguild.org>
2016-06-09 14:50:39 -0700

    fix rethink truncate timeouts on CircleCI

--- test/helpers/db.js
+++ test/helpers/db.js
@@ -1,9 +1,13 @@
 import r from '../../db/connect'
 
 function truncateDBTables() {
+  this.timeout(30000)  // for some reason, truncating tables can sometimes take a long time
   return r.tableList()
     .then(tables => tables.filter(t => !t.startsWith('_')))
     .then(tablesToTruncate => Promise.all(tablesToTruncate.map(t => r.table(t).delete().run())))
+    .then(() => {
+      this.timeout(2000)  // reset to default timeout
+    })
 }
 
 /* eslint-env mocha */
