c61b7e96ea57e6ade4ad4e415214629329b281b6
jeffreywescott <jeffrey@learnersguild.org>
2016-06-17 11:15:35 -0700

    try to truncate tables serially rather than in parallel

--- test/helpers/db.js
+++ test/helpers/db.js
@@ -1,13 +1,11 @@
+import Promise from 'bluebird'
+
 import r from '../../db/connect'
 
 function truncateDBTables() {
-  this.timeout(30000)  // for some reason, truncating tables can sometimes take a long time
   return r.tableList()
     .then(tables => tables.filter(t => !t.startsWith('_')))
-    .then(tablesToTruncate => Promise.all(tablesToTruncate.map(t => r.table(t).delete().run())))
-    .then(() => {
-      // this.timeout(2000)  // reset to default timeout
-    })
+    .then(tablesToTruncate => Promise.each(tablesToTruncate, t => r.table(t).delete().run()))
 }
 
 /* eslint-env mocha */
