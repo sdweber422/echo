ba79be7d2c6c4d91cc69889aba8b5b4156f8fc1b
jeffreywescott <jeffrey@learnersguild.org>
2016-06-17 14:13:05 -0700

    log connection info when truncating

--- test/helpers/db.js
+++ test/helpers/db.js
@@ -3,6 +3,7 @@ import Promise from 'bluebird'
 import r from '../../db/connect'
 
 function truncateDBTables() {
+  console.log('db connections (avail / total):', r.getPoolMaster().getAvailableLength(), '/', r.getPoolMaster().getLength())
   return r.tableList()
     .then(tables => tables.filter(t => !t.startsWith('_')))
     .then(tablesToTruncate => Promise.each(tablesToTruncate, t => r.table(t).delete().run()))
