39e5d30ea0abe045118d09c870a107418d2d494c
Trevor Little <trevor@learnersguild.org>
2016-06-23 11:45:13 -0400

    removing random data from chapter factory.

--- test/factories/chapter.js
+++ test/factories/chapter.js
@@ -1,16 +1,15 @@
 import faker from 'faker'
 import r from '../../db/connect'
 
-const VALID_TIMEZONES = ['America/Los_Angeles', 'America/Chicago', 'America/New_York']
 const now = new Date()
 
 export default function define(factory) {
   factory.define('chapter', r.table('chapters'), {
     id: cb => cb(null, faker.random.uuid()),
-    name: cb => cb(null, faker.address.city()),
+    name: factory.sequence(n => `Test Chapter ${n}`),
     /* eslint-disable babel/object-shorthand, brace-style */
     channelName: function (cb) { cb(null, faker.helpers.slugify(this.name).toLowerCase()) },
-    timezone: cb => cb(null, faker.random.arrayElement(VALID_TIMEZONES)),
+    timezone: 'America/Los_Angeles',
     goalRepositoryURL: cb => cb(null, 'https://github.com/GuildCraftsTesting/web-development-js-testing'),
     githubTeamId: cb => cb(null, null),
     cycleDuration: '1 week',
