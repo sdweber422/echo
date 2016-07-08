248adb1c34b22d903e5bd45fc8edf79af584f0d0
Trevor Little <trevor@learnersguild.org>
2016-07-08 15:19:25 -0600

    ignoring out-of-order cycle state changes fixing playest script.

--- server/configureChangeFeeds/cycleStateChanged.js
+++ server/configureChangeFeeds/cycleStateChanged.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console, camelcase */
 import raven from 'raven'
 import r from '../../db/connect'
+import {CYCLE_STATES} from '../../common/models/cycle'
 
 const sentry = new raven.Client(process.env.SENTRY_SERVER_DSN)
 
@@ -20,8 +21,17 @@ export default function cycleStateChanged(cycleStateChangedQueues) {
         const previousState = cycleBeforeChange ? cycleBeforeChange.state : '(not yet created)'
         console.log(`Cycle state for ${cycle.id} changed from ${previousState} to ${cycle.state}`)
         const queue = cycleStateChangedQueues[cycle.state]
+
         if (queue) {
-          queue.add(cycle)
+          const previousStateIndex = cycleBeforeChange ? CYCLE_STATES.indexOf(cycleBeforeChange.state) : -1
+          const newStateIndex = CYCLE_STATES.indexOf(cycle.state)
+          const stateChanegIsInOrder = previousStateIndex === newStateIndex - 1
+
+          if (stateChanegIsInOrder) {
+            queue.add(cycle)
+          } else {
+            console.log('Ignoring out of order state change; not adding job to queue')
+          }
         }
       })
     })
