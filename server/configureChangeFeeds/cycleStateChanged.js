9074bc8afc2e68adaf0b89ad29a53cabb5d4b70d
jeffreywescott <jeffrey@learnersguild.org>
2016-07-09 08:43:29 -0700

    integrating my own PR feedback

--- server/configureChangeFeeds/cycleStateChanged.js
+++ server/configureChangeFeeds/cycleStateChanged.js
@@ -18,16 +18,19 @@ export default function cycleStateChanged(cycleStateChangedQueues) {
           sentry.captureException(err)
           return
         }
+        // if there was no previous cycle, that means this is a new cycle
         const previousState = cycleBeforeChange ? cycleBeforeChange.state : '(not yet created)'
         console.log(`Cycle state for ${cycle.id} changed from ${previousState} to ${cycle.state}`)
         const queue = cycleStateChangedQueues[cycle.state]
 
         if (queue) {
+          // setting the previous state index to -1 in the case that this is a new cycle ensures that
+          // stateChangeIsInOrder will evaluate to true below
           const previousStateIndex = cycleBeforeChange ? CYCLE_STATES.indexOf(cycleBeforeChange.state) : -1
           const newStateIndex = CYCLE_STATES.indexOf(cycle.state)
-          const stateChanegIsInOrder = previousStateIndex === newStateIndex - 1
+          const stateChangeIsInOrder = previousStateIndex === newStateIndex - 1
 
-          if (stateChanegIsInOrder) {
+          if (stateChangeIsInOrder) {
             queue.add(cycle)
           } else {
             console.log('Ignoring out of order state change; not adding job to queue')
