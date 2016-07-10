639d79b688a34c5ddd6144061566f695ca497acc
jeffreywescott <jeffrey@learnersguild.org>
2016-07-10 14:27:58 -0700

    other services being down shouldn't break this service
    For sliding sessions, we try to contact IDM to update our user record. If the IDM service is down, however, this middleware will fail. But that shouldn't necessarily cause an error in this service if our JWT hasn't yet expired. So we'll log the error and move on.

--- server/auth.js
+++ server/auth.js
@@ -1,4 +1,6 @@
+
 import express from 'express'
+import raven from 'raven'
 
 import {
   addUserToRequestFromJWT,
@@ -6,12 +8,24 @@ import {
   refreshUserFromIDMService
 } from '@learnersguild/idm-jwt-auth/lib/middlewares'
 
-/* eslint babel/new-cap: [2, {"capIsNewExceptions": ["Router"]}] */
+const sentry = new raven.Client(process.env.SENTRY_SERVER_DSN)
+
 const app = new express.Router()
 
 // app configuration
 app.use(addUserToRequestFromJWT)
-app.use(refreshUserFromIDMService)
+app.use((req, res, next) => {
+  refreshUserFromIDMService(req, res, err => {
+    if (err) {
+      // this is not enough to break things -- if we are unable to refresh the
+      // user from IDM, but our JWT is still valid, it's okay, so we won't
+      // allow this error to propagate beyond this point
+      console.warn('WARNING: unable to refresh user from IDM service:', err)
+      sentry.captureException(err)
+    }
+    next()
+  })
+})
 app.use(extendJWTExpiration)
 
 export default app
