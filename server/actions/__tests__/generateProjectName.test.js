12231b56322cfe18fa50b52334d8923bbb1fc36f
SJ Pratt <pratt.sabra@gmail.com>
2016-07-28 11:55:12 -0700

    split up project formation tests

---
+++ server/actions/__tests__/generateProjectName.test.js
@@ -0,0 +1,17 @@
+/* eslint-env mocha */
+/* global testContext */
+/* eslint-disable prefer-arrow-callback, no-unused-expressions, max-nested-callbacks */
+
+import {assert} from 'chai'
+
+import generateProjectName from '../generateProjectName'
+
+describe(testContext(__filename), function () {
+  describe('generateProjectName()', function () {
+    it('generates a valid project name', function () {
+      return generateProjectName().then(function (projectName) {
+        assert.match(projectName, /^\w+(-\w+)+(-\d)?$/)
+      })
+    })
+  })
+})
--- server/actions/formProjects.js
+++ server/actions/formProjects.js
@@ -13,14 +13,15 @@
 import {getCycleById} from '../db/cycle'
 import {findPlayersByIds} from '../db/player'
 import {findVotesForCycle} from '../db/vote'
-import {insertProjects, findProjects} from '../db/project'
+import {insertProjects} from '../db/project'
 import {toArray} from '../util'
-import randomMemorableName from '../../common/util/randomMemorableName'
+import {createTeamSizes} from '../util/project'
+import generateProjectName from './generateProjectName'
 
 const MIN_ADVANCED_PLAYER_ECC = 1000
 const DEFAULT_RECOMMENDED_TEAM_SIZE = 5
 
-async function _formProjects(cycleId) {
+export default async function formProjects(cycleId) {
   const cycle = await getCycleById(cycleId)
 
   const cycleVotes = await findVotesForCycle(cycleId).run()
@@ -219,7 +220,7 @@ function _arrangePlayerTeams(recTeamSize, regularPlayers, advancedPlayers) {
   const rankedRegularPlayers = _rankPlayers(regularPlayers)
   const rankedAdvancedPlayers = _rankPlayers(advancedPlayers)
 
-  const teamSizes = _getTeamSizes(recTeamSize, rankedRegularPlayers.length, rankedAdvancedPlayers.length)
+  const teamSizes = createTeamSizes(recTeamSize, rankedRegularPlayers.length, rankedAdvancedPlayers.length)
   const regularTeamPlayers = _playersForTeamSizes(teamSizes.map(teamSize => teamSize.regular), rankedRegularPlayers)
   const advancedTeamPlayers = _playersForTeamSizes(teamSizes.map(teamSize => teamSize.advanced), rankedAdvancedPlayers)
 
@@ -229,61 +230,6 @@ function _arrangePlayerTeams(recTeamSize, regularPlayers, advancedPlayers) {
     advancedTeamPlayers[i].forEach(p => mergedPlayers.set(p.id, p))
     return Array.from(mergedPlayers.values())
   })
-}
-
-function _getTeamSizes(recTeamSize, numRegularPlayers, numAdvancedPlayers) {
-  const numPerfectRegularPlayers = recTeamSize - 1 // leave room for exactly 1 advanced player
-  const numPerfectTeams = Math.floor(numRegularPlayers / numPerfectRegularPlayers)
-
-  // form as many perfect teams as possible
-  const teamSizes = new Array(numPerfectTeams).fill(null).map(() => ({regular: numPerfectRegularPlayers, advanced: 1}))
-
-  // any regular or advanced players "left over"?
-  const remainingRegularPlayers = (numRegularPlayers % numPerfectRegularPlayers) || 0
-  const remainingAdvancedPlayers = Math.max(numAdvancedPlayers - teamSizes.length, 0)
-  const totalRemaining = remainingRegularPlayers + remainingAdvancedPlayers
-  const maxRemaining = remainingAdvancedPlayers ? totalRemaining : (remainingRegularPlayers + 1)
-
-  if (totalRemaining) {
-    const remainingTeamSize = {regular: remainingRegularPlayers, advanced: remainingAdvancedPlayers}
-    const minTeamSize = recTeamSize - 1
-    const maxTeamSize = recTeamSize + 1
-
-    if (maxRemaining >= minTeamSize && maxRemaining <= maxTeamSize) {
-      if (!remainingAdvancedPlayers) {
-        remainingTeamSize.advanced = 1
-      }
-      teamSizes.push(remainingTeamSize)
-    } else if (totalRemaining <= teamSizes.length) {
-      // teams can be rec size + 1, and there are few enough remaining spots that
-      // we can add each of them to an existing (previously "perfect-sized") team
-      let i = 0
-      for (; i < remainingRegularPlayers; i++) {
-        teamSizes[i].regular++
-      }
-      for (let j = 0; j < remainingAdvancedPlayers; i++, j++) {
-        teamSizes[j].advanced++
-      }
-    } else if ((minTeamSize - maxRemaining) <= teamSizes.length) {
-      // teams can be rec size - 1, and there are enough "perfect-sized" teams
-      // that we can take 1 spot from the regular players of some them and
-      // add those to the leftover spots to make 1 more team
-      if (!remainingTeamSize.advanced) {
-        remainingTeamSize.advanced = 1 // ensure that at least 1 advanced player is pulled onto this team
-      }
-      for (let i = 0; (remainingTeamSize.regular + remainingTeamSize.advanced) < minTeamSize; i++) {
-        teamSizes[i].regular--
-        remainingTeamSize.regular++
-      }
-      teamSizes.push(remainingTeamSize)
-    } else {
-      // make a team out of the remaining spots anyway
-      // TODO: throw an error? toss the entire goal group? do something better.
-      teamSizes.push(remainingTeamSize)
-    }
-  }
-
-  return teamSizes
 }
 
 function _playersForTeamSizes(teamSizes, players) {
@@ -308,7 +254,7 @@ function _formProjectsForGoalGroups(chapterId, cycleId, goalGroups) {
   goalGroups.forEach(goalGroup => {
     goalGroup.teams.forEach(teamPlayers => {
       projects.push(
-        _generateProjectName().then(name => {
+        generateProjectName().then(name => {
           return {
             chapterId,
             name,
@@ -325,14 +271,3 @@ function _formProjectsForGoalGroups(chapterId, cycleId, goalGroups) {
 
   return Promise.all(projects)
 }
-
-function _generateProjectName() {
-  const projectName = randomMemorableName()
-  return findProjects({name: projectName}).run().then(existingProjectsWithName => {
-    return existingProjectsWithName.length ? _generateProjectName() : projectName
-  })
-}
-
-export const formProjects = _formProjects
-export const getTeamSizes = _getTeamSizes
-export const generateProjectName = _generateProjectName
