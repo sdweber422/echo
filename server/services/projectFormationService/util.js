a06fd093642d47e38a40f9029c36b7d0d3851951
Trevor Little <trevor@learnersguild.org>
2016-09-22 09:53:25 -0400

    consolidating util files into 1

--- server/services/projectFormationService/util.js
+++ server/services/projectFormationService/util.js
@@ -1,44 +1,8 @@
-export function unique(array) {
-  return Array.from(new Set(array))
-}
-
-export function flatten(potentialArray) {
-  if (!Array.isArray(potentialArray)) {
-    return potentialArray
-  }
-  return potentialArray.reduce((result, next) => result.concat(flatten(next)), [])
-}
-
-export function range(start, length) {
-  return Array.from(Array(length), (x, i) => i + start)
-}
-
-export function repeat(length, element) {
-  return Array.from(Array(length), () => element)
-}
-
-// https://en.wikipedia.org/wiki/Combination
-export function choose(n, k) {
-  if (k === 0) {
-    return 1
-  }
-  return (n * choose(n - 1, k - 1)) / k
-}
-
-export function shuffle(array) {
-  let currentIndex = array.length
-
-  // While there remain elements to shuffle...
-  while (currentIndex !== 0) {
-    // Pick a remaining element...
-    const randomIndex = Math.floor(Math.random() * currentIndex)
-    currentIndex -= 1
-
-    // And swap it with the current element.
-    const temporaryValue = array[currentIndex]
-    array[currentIndex] = array[randomIndex]
-    array[randomIndex] = temporaryValue
-  }
-
-  return array
-}
+export {
+  unique,
+  flatten,
+  range,
+  repeat,
+  choose,
+  shuffle,
+} from 'src/server/util'
