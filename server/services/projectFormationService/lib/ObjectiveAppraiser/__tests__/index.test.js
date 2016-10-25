3a058a6022eb34b3fe4f820adc8e2e5f7b6c2579
Trevor Little <trevor@learnersguild.org>
2016-10-25 09:35:12 -0400

    a little more test cleanup.
    using buildTestPool in favor of hard cocded pools where possible. Since
    this functions creates goals starting with `g0` instead of `g1` I had to
    change the goals in the team formation plans to match.

--- server/services/projectFormationService/lib/ObjectiveAppraiser/__tests__/index.test.js
+++ server/services/projectFormationService/lib/ObjectiveAppraiser/__tests__/index.test.js
@@ -2,6 +2,7 @@
 /* global expect, testContext */
 /* eslint-disable prefer-arrow-callback, no-unused-expressions, max-nested-callbacks */
 
+import {buildTestPool} from '../../../__tests__/helpers'
 import ObjectiveAppraiser from '../index'
 
 describe(testContext(__filename), function () {
@@ -55,85 +56,64 @@ describe(testContext(__filename), function () {
     })
 
     context('when the objectives are specified in the pool', function () {
-      const pool = {
-        votes: [
-          {playerId: 'A0', votes: ['g1', 'g2']},
-          {playerId: 'p0', votes: ['g1', 'g2']},
-          {playerId: 'A1', votes: ['g1', 'g2']},
-          {playerId: 'p1', votes: ['g1', 'g2']},
-          {playerId: 'p2', votes: ['g1', 'g2']},
-          {playerId: 'p3', votes: ['g1', 'g2']},
-        ],
-        goals: [
-          {goalDescriptor: 'g1', teamSize: 3},
-          {goalDescriptor: 'g2', teamSize: 3},
-          {goalDescriptor: 'g3', teamSize: 3},
-        ],
-        advancedPlayers: [{id: 'A0', maxTeams: 2}, {id: 'A1', maxTeams: 1}],
-      }
-
-      const poolWithObjectives = ({advancedWeight, nonAdvancedWeight}) => ({
-        ...pool,
-        objectives: {
-          mandatory: [],
-          weighted: [
-            ['NonAdvancedPlayersGotTheirVote', nonAdvancedWeight],
-            ['AdvancedPlayersGotTheirVote', advancedWeight],
-          ],
-        }
+      const pool = buildTestPool({
+        playerCount: 4,
+        advancedPlayerCount: 2,
+        goalCount: 3,
+        teamSize: 3,
+        voteDistributionPercentages: [1],
       })
-
-      const scoreWithPool = (teamFormationPlan, pool) => {
-        const appraiser = new ObjectiveAppraiser(pool)
-        return appraiser.score(teamFormationPlan)
-      }
-
-      const itReturnsTheCorrectScore = ({
-        advancedWeight,
-        nonAdvancedWeight,
-        percentageAdvancedPlayersGotTheirVote,
-        percentageNonAdvancedPlayersGotTheirVote,
-        teamFormationPlan,
-      }) => {
-        const pool = poolWithObjectives({advancedWeight, nonAdvancedWeight})
-        const sumOfWeights = advancedWeight + nonAdvancedWeight
-        expect(scoreWithPool(teamFormationPlan, pool)).to.eq(
-          (
-            percentageNonAdvancedPlayersGotTheirVote * nonAdvancedWeight +
-            percentageAdvancedPlayersGotTheirVote * advancedWeight
-          ) / sumOfWeights
-        )
-      }
 
       const teamFormationPlan = {
         seatCount: 6,
         teams: [
-          {goalDescriptor: 'g1', teamSize: 2, playerIds: ['A0', 'p0']},
-          {goalDescriptor: 'g3', teamSize: 4, playerIds: ['A1', 'p1', 'p2', 'p3']},
+          {goalDescriptor: 'g0', teamSize: 2, playerIds: ['A0', 'p0']},
+          {goalDescriptor: 'g2', teamSize: 4, playerIds: ['A1', 'p1', 'p2', 'p3']},
         ],
       }
 
       const percentageAdvancedPlayersGotTheirVote = 1 / 2
       const percentageNonAdvancedPlayersGotTheirVote = 1 / 4
 
+      const scoreWithPool = pool => {
+        const appraiser = new ObjectiveAppraiser(pool)
+        return appraiser.score(teamFormationPlan)
+      }
+
       it('returns a perfect score if there are no weighted objectives', function () {
         const poolWithNoWeightedObjectives = {...pool, objectives: {weighted: []}}
-        expect(scoreWithPool(teamFormationPlan, poolWithNoWeightedObjectives)).to.eq(1)
+        expect(scoreWithPool(poolWithNoWeightedObjectives)).to.eq(1)
       })
 
       it('uses the weights', function () {
+        const poolWithObjectives = ({advancedWeight, nonAdvancedWeight}) => ({
+          ...pool,
+          objectives: {
+            mandatory: [],
+            weighted: [
+              ['NonAdvancedPlayersGotTheirVote', nonAdvancedWeight],
+              ['AdvancedPlayersGotTheirVote', advancedWeight],
+            ],
+          }
+        })
+
+        const itReturnsTheCorrectScore = ({advancedWeight, nonAdvancedWeight}) => {
+          const pool = poolWithObjectives({advancedWeight, nonAdvancedWeight})
+          const sumOfWeights = advancedWeight + nonAdvancedWeight
+          expect(scoreWithPool(pool)).to.eq(
+            (
+              percentageNonAdvancedPlayersGotTheirVote * nonAdvancedWeight +
+              percentageAdvancedPlayersGotTheirVote * advancedWeight
+            ) / sumOfWeights
+          )
+        }
+
         [
           {advancedWeight: 1, nonAdvancedWeight: 2},
           {advancedWeight: 1, nonAdvancedWeight: 1},
           {advancedWeight: 100, nonAdvancedWeight: 1},
         ].forEach(({advancedWeight, nonAdvancedWeight}) => {
-          itReturnsTheCorrectScore({
-            advancedWeight,
-            nonAdvancedWeight,
-            percentageAdvancedPlayersGotTheirVote,
-            percentageNonAdvancedPlayersGotTheirVote,
-            teamFormationPlan,
-          })
+          itReturnsTheCorrectScore({advancedWeight, nonAdvancedWeight})
         })
       })
     })
@@ -141,41 +121,31 @@ describe(testContext(__filename), function () {
 
   describe('.objectiveScores()', function () {
     it('returns the individual scored for each objective', function () {
-      const pool = {
-        votes: [
-          {playerId: 'A0', votes: ['g1', 'g2']},
-          {playerId: 'p0', votes: ['g1', 'g2']},
-          {playerId: 'p1', votes: ['g1', 'g2']},
-          {playerId: 'A1', votes: ['g2', 'g1']},
-          {playerId: 'p2', votes: ['g2', 'g2']},
-          {playerId: 'p3', votes: ['g2', 'g1']},
-          {playerId: 'p4', votes: ['g2', 'g1']},
-          {playerId: 'p5', votes: ['g2', 'g1']},
-        ],
-        goals: [
-          {goalDescriptor: 'g1', teamSize: 3},
-          {goalDescriptor: 'g2', teamSize: 3},
-        ],
-        advancedPlayers: [{id: 'A0'}, {id: 'A1', maxTeams: 1}],
-      }
+      const pool = buildTestPool({
+        playerCount: 6,
+        advancedPlayerCount: 2,
+        goalCount: 2,
+        teamSize: 3,
+        voteDistributionPercentages: [0.3, 0.7],
+      })
 
       const teamFormationPlan = {
         seatCount: 9,
         teams: [
           {
-            goalDescriptor: 'g1',
+            goalDescriptor: 'g0',
             teamSize: 3,
             matchesTeamSizeRecommendation: true,
             playerIds: ['A0', 'p0', 'p1']
           },
           {
-            goalDescriptor: 'g1',
+            goalDescriptor: 'g0',
             teamSize: 3,
             matchesTeamSizeRecommendation: true,
             playerIds: ['A0', 'p3', 'p4']
           },
           {
-            goalDescriptor: 'g2',
+            goalDescriptor: 'g1',
             teamSize: 3,
             matchesTeamSizeRecommendation: true,
             playerIds: ['A1', 'p2', 'p5']
