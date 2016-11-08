1d30f81f5b0637afdaef82395c8501fb610e713e
jeffreywescott <jeffrey@learnersguild.org>
2016-11-08 14:21:23 -0800

    `newChapters`: use `processChangeFeedWithAutoReconnect`

--- server/configureChangeFeeds/newChapters.js
+++ server/configureChangeFeeds/newChapters.js
@@ -1,26 +1,25 @@
 /* eslint-disable no-console, camelcase */
-import raven from 'raven'
-import config from 'src/config'
 import {connect} from 'src/db'
 
+import {processChangeFeedWithAutoReconnect} from './util'
+
 const r = connect()
-const sentry = new raven.Client(config.server.sentryDSN)
 
 export default function newChapters(newChapterQueue) {
-  r.table('chapters').changes().filter(r.row('old_val').eq(null))
-    .then(cursor => {
-      cursor.each((err, {new_val: chapter}) => {
-        if (err) {
-          console.error(err)
-          sentry.captureException(err)
-          return
-        }
+  processChangeFeedWithAutoReconnect(_getFeed, _getFeedProcessor(newChapterQueue), 'new chapters')
+}
 
-        const jobOpts = {
-          attempts: 3,
-          backoff: {type: 'fixed', delay: 60000},
-        }
-        newChapterQueue.add(chapter, jobOpts)
-      })
-    })
+function _getFeed() {
+  return r.table('chapters').changes()
+    .filter(r.row('old_val').eq(null))
+}
+
+function _getFeedProcessor(newChapterQueue) {
+  return ({new_val: chapter}) => {
+    const jobOpts = {
+      attempts: 3,
+      backoff: {type: 'fixed', delay: 60000},
+    }
+    newChapterQueue.add(chapter, jobOpts)
+  }
 }
