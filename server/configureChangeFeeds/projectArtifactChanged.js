18c7addee5895569fc08135e49cbb28390e1fe65
jeffreywescott <jeffrey@learnersguild.org>
2016-11-08 14:22:31 -0800

    `projectArtifactChanged`: use `processChangeFeedWithAutoReconnect`
    Also fix bug: if the artifactURL wasn't previously set, no notification would happen

--- server/configureChangeFeeds/projectArtifactChanged.js
+++ server/configureChangeFeeds/projectArtifactChanged.js
@@ -1,27 +1,35 @@
 /* eslint-disable no-console, camelcase */
-import raven from 'raven'
-import config from 'src/config'
 import {connect} from 'src/db'
 
+import {processChangeFeedWithAutoReconnect} from './util'
+
 const r = connect()
-const sentry = new raven.Client(config.server.sentryDSN)
 
 export default function projectArtifactChanged(projectArtifactChangedQueue) {
-  r.table('projects').changes()
-    .filter(r.row('new_val')('artifactURL').ne(r.row('old_val')('artifactURL')))
-    .then(cursor => {
-      cursor.each((err, {new_val: project}) => {
-        if (err) {
-          console.error(err)
-          sentry.captureException(err)
-          return
-        }
+  processChangeFeedWithAutoReconnect(_getFeed, _getFeedProcessor(projectArtifactChangedQueue), 'project artifact changed')
+}
 
-        const jobOpts = {
-          attempts: 3,
-          backoff: {type: 'fixed', delay: 10000},
-        }
-        projectArtifactChangedQueue.add(project, jobOpts)
-      })
+function _getFeed() {
+  return r.table('projects').changes()
+    .filter(row => {
+      const origProject = row('old_val')
+      const project = row('new_val')
+      return r.or(
+        r.and(
+          r.not(origProject.hasFields('artifactURL')),
+          project.hasFields('artifactURL')
+        ),
+        project('artifactURL').ne(origProject('artifactURL'))
+      )
     })
+}
+
+function _getFeedProcessor(projectArtifactChangedQueue) {
+  return ({new_val: project}) => {
+    const jobOpts = {
+      attempts: 3,
+      backoff: {type: 'fixed', delay: 10000},
+    }
+    projectArtifactChangedQueue.add(project, jobOpts)
+  }
 }
