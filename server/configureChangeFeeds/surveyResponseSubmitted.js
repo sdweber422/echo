814f7d4c9cad3ef06b8b6a58ef1364022cb9a01e
jeffreywescott <jeffrey@learnersguild.org>
2016-11-08 14:23:28 -0800

    `surveyResponseSubmitted`: use `processChangeFeedWithAutoReconnect`

--- server/configureChangeFeeds/surveyResponseSubmitted.js
+++ server/configureChangeFeeds/surveyResponseSubmitted.js
@@ -1,33 +1,16 @@
 /* eslint-disable no-console, camelcase */
-import raven from 'raven'
-import config from 'src/config'
 import {connect} from 'src/db'
 import {responsesTable} from 'src/server/db/response'
 
+import {processChangeFeedWithAutoReconnect} from './util'
+
 const r = connect()
-const sentry = new raven.Client(config.server.sentryDSN)
 
 export default function surveyResponseSubmitted(queue) {
-  surveyResponsesFeed()
-    .pluck('respondentId', 'surveyId')
-    .then(cursor => {
-      cursor.each((err, responseInfo) => {
-        if (err) {
-          console.error(err)
-          sentry.captureException(err)
-          return
-        }
-
-        const jobOpts = {
-          attempts: 3,
-          backoff: {type: 'fixed', delay: 10000},
-        }
-        queue.add(responseInfo, jobOpts)
-      })
-    })
+  processChangeFeedWithAutoReconnect(_getFeed, _getFeedProcessor(queue), 'survey response submitted')
 }
 
-function surveyResponsesFeed() {
+function _getFeed() {
   return responsesTable.changes()
     .filter(
       r.or(
@@ -35,5 +18,14 @@ function surveyResponsesFeed() {
         r.row('new_val')('value').ne(r.row('old_val')('value'))
       )
     )
-    .map(changes => changes('new_val'))
+}
+
+function _getFeedProcessor(queue) {
+  return ({new_val: responseInfo}) => {
+    const jobOpts = {
+      attempts: 3,
+      backoff: {type: 'fixed', delay: 10000},
+    }
+    queue.add(responseInfo, jobOpts)
+  }
 }
