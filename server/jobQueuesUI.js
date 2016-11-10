06e7e68fd18103d594e96d5b7d042da01259dd98
jeffreywescott <jeffrey@learnersguild.org>
2016-11-10 13:59:52 -0800

    hotfix: fix redis connection in prod

--- server/jobQueuesUI.js
+++ server/jobQueuesUI.js
@@ -7,7 +7,7 @@ import {userCan} from 'src/common/util'
 
 const app = new express.Router()
 const redisConfig = url.parse(config.server.redis.url)
-const redisPasswordOpts = redisConfig.auth ? {password: redisConfig.auth.split(':')[1]} : {}
+const redisPasswordOpts = redisConfig.auth ? {auth_pass: redisConfig.auth.split(':')[1]} : {} // eslint-disable-line camelcase
 const toureiroApp = toureiro({
   redis: {
     host: redisConfig.hostname,
