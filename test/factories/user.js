826fcdecddbbc519704aa9e97e1ef4b1052a90f9
jeffreywescott <jeffrey@learnersguild.org>
2016-11-16 12:45:37 -0800

    add avatarUrl to user factory

--- test/factories/user.js
+++ test/factories/user.js
@@ -9,6 +9,7 @@ export default function define(factory) {
     email: cb => cb(null, faker.internet.exampleEmail()),
     emails: cb => cb(null, [faker.internet.exampleEmail(), faker.internet.exampleEmail()]),
     handle: cb => cb(null, `${faker.random.word()}${faker.random.number({max: 100})}`.toLowerCase()),
+    avatarUrl: cb => cb(null, faker.image.imageUrl()),
     name: cb => cb(null, faker.name.findName()),
     phone: cb => cb(null, faker.phone.phoneNumber('(###) ###-####')),
     dateOfBirth: cb => cb(null, faker.date.past(21).toISOString().slice(0, 10)),
