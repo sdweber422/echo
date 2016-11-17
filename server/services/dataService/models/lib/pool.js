abb751f13a59a18089464acd3cf11e20be828e41
Trevor Little <trevor@learnersguild.org>
2016-11-17 10:22:18 -0500

    adding thinky pool model

---
+++ server/services/dataService/models/lib/pool.js
@@ -0,0 +1,30 @@
+import {type} from 'thinky'
+
+const {string, date} = type
+
+export default {
+  name: 'Pool',
+  table: 'pools',
+  schema: {
+    id: string()
+      .uuid(4)
+      .allowNull(false),
+
+    name: string()
+      .required()
+      .allowNull(false),
+
+    createdAt: date()
+      .required()
+      .allowNull(false)
+      .default(new Date()),
+
+    updatedAt: date()
+      .required()
+      .allowNull(false)
+      .default(new Date()),
+  },
+  associate: (Vote, models) => {
+    Vote.belongsTo(models.Cycle, 'cycle', 'cycleId', 'id', {init: false})
+  },
+}
