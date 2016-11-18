5a03b8eabfc249b24fe82d229dcdfa86c7d34cae
jeffreywescott <jeffrey@learnersguild.org>
2016-11-18 11:44:42 -0800

    re-order stats for easier understanding

--- common/models/stat.js
+++ common/models/stat.js
@@ -1,13 +1,15 @@
 export const STAT_DESCRIPTORS = {
   RELATIVE_CONTRIBUTION: 'relativeContribution',
   TECHNICAL_HEALTH: 'technicalHealth',
-  TEAM_PLAY: 'teamPlay',
-  CULTURE_CONTRIBUTION: 'cultureContribution',
   PROJECT_HOURS: 'projectHours',
+  CHALLENGE: 'challenge',
   GENERAL_FEEDBACK: 'generalFeedback',
+  CULTURE_CONTRIBUTION: 'cultureContribution',
+
+  // team play stats
+  TEAM_PLAY: 'teamPlay',
   RECEPTIVENESS: 'receptiveness',
   RESULTS_FOCUS: 'resultsFocus',
   FLEXIBLE_LEADERSHIP: 'flexibleLeadership',
   FRICTION_REDUCTION: 'frictionReduction',
-  CHALLENGE: 'challenge',
 }
