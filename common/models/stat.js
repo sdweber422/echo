a8bfd1ee227ada42a1f0a7cbbdd8b39edd0514cb
jeffreywescott <jeffrey@learnersguild.org>
2016-11-18 11:53:23 -0800

    add new culture contribution stats data

--- common/models/stat.js
+++ common/models/stat.js
@@ -4,7 +4,16 @@ export const STAT_DESCRIPTORS = {
   PROJECT_HOURS: 'projectHours',
   CHALLENGE: 'challenge',
   GENERAL_FEEDBACK: 'generalFeedback',
+
+  // culture contribution stats
   CULTURE_CONTRIBUTION: 'cultureContribution',
+  CULTURE_CONTRIBUTION_STRUCTURE: 'cultureContributionStructure',
+  CULTURE_CONTRIBUTION_SAFETY: 'cultureContributionSafety',
+  CULTURE_CONTRIBUTION_TRUTH: 'cultureContributionTruth',
+  CULTURE_CONTRIBUTION_CHALLENGE: 'cultureContributionChallenge',
+  CULTURE_CONTRIBUTION_SUPPORT: 'cultureContributionSupport',
+  CULTURE_CONTRIBUTION_ENGAGEMENT: 'cultureContributionEngagement',
+  CULTURE_CONTRIBUTION_ENJOYMENT: 'cultureContributionEnjoyment',
 
   // team play stats
   TEAM_PLAY: 'teamPlay',
