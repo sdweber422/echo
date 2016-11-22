5f2453f7585f75fd84ccc84d132609a86a8d4085
Trevor Little <trevor@learnersguild.org>
2016-11-22 11:40:43 -0500

    removing duplication in test

--- server/graphql/models/Vote/__tests__/query.test.js
+++ server/graphql/models/Vote/__tests__/query.test.js
@@ -14,22 +14,19 @@ describe(testContext(__filename), function () {
     beforeEach(async function () {
       this.chapter = await factory.create('chapter')
       this.cycle = await factory.create('cycle', {chapterId: this.chapter.id})
-      const [pool1, pool2] = await factory.createMany('pool', {cycleId: this.cycle.id}, 2)
-      this.pool1 = pool1
-      this.pool2 = pool2
-      this.pools = [pool1, pool2]
+      this.pools = await factory.createMany('pool', {cycleId: this.cycle.id}, 2)
 
-      this.pool1Voters = []
-      this.pool1Players = await factory.createMany('player', {chapterId: this.chapter.id}, 3)
-      await addPlayerIdsToPool(pool1.id, this.pool1Players.map(_ => _.id))
-
-      this.pool2Voters = []
-      this.pool2Players = await factory.createMany('player', {chapterId: this.chapter.id}, 3)
-      await addPlayerIdsToPool(pool2.id, this.pool2Players.map(_ => _.id))
-
-      this.poolPlayers = [this.pool1Players, this.pool2Players]
-
-      this.players = this.pool1Players.concat(this.pool2Players)
+      this.poolVoters = this.pools.map(_ => [])
+      this.poolPlayers = []
+      this.players = []
+      /* eslint-disable babel/no-await-in-loop */
+      for (const pool of this.pools) {
+        const players = await factory.createMany('player', {chapterId: this.chapter.id}, 3)
+        await addPlayerIdsToPool(pool.id, players.map(_ => _.id))
+        this.poolPlayers.push(players)
+        this.players.push(...players)
+      }
+      /* eslint-enable babel/no-await-in-loop */
 
       this.currentUser = await factory.build('user', {id: this.players[0].id})
       this.moderator = await factory.create('moderator', {id: this.currentUser.id})
@@ -66,82 +63,63 @@ describe(testContext(__filename), function () {
     }
 
     describe('when there are votes', function () {
-      const voteDataForPool1 = {
-        firstPlaceGoalNumber: 1,
-        secondPlaceGoalNumber: 2,
-        thirdPlaceGoalNumber: 3,
-        goalNumberVotes: [
-          [1, 2],
-          [1, 2],
-          [1, 3],
-        ]
-      }
-      const voteDataForPool2 = {
-        firstPlaceGoalNumber: 101,
-        secondPlaceGoalNumber: 102,
-        thirdPlaceGoalNumber: 103,
-        goalNumberVotes: [
-          [101, 102],
-          [101, 102],
-          [101, 103],
-        ]
-      }
+      const voteDataForPools = [
+        {
+          firstPlaceGoalNumber: 1,
+          secondPlaceGoalNumber: 2,
+          thirdPlaceGoalNumber: 3,
+          goalNumberVotes: [
+            [1, 2],
+            [1, 2],
+            [1, 3],
+          ]
+        },
+        {
+          firstPlaceGoalNumber: 101,
+          secondPlaceGoalNumber: 102,
+          thirdPlaceGoalNumber: 103,
+          goalNumberVotes: [
+            [101, 102],
+            [101, 102],
+            [101, 103],
+          ]
+        }
+      ]
 
       beforeEach('create some votes', async function () {
-        await Promise.all(this.pool1Players.map((player, i) => {
-          const [goal1, goal2] = voteDataForPool1.goalNumberVotes[i]
-          return factory.create('vote', {
-            playerId: player.id,
-            poolId: this.pool1.id,
-            goals: [
-              {url: `${this.chapter.goalRepositoryURL}/issues/${goal1}`},
-              {url: `${this.chapter.goalRepositoryURL}/issues/${goal2}`},
-            ],
+        await Promise.map(this.pools, async (pool, i) => {
+          await Promise.map(this.poolPlayers[i], (player, j) => {
+            const [goal1, goal2] = voteDataForPools[i].goalNumberVotes[j]
+            return factory.create('vote', {
+              playerId: player.id,
+              poolId: pool.id,
+              goals: [
+                {url: `${this.chapter.goalRepositoryURL}/issues/${goal1}`},
+                {url: `${this.chapter.goalRepositoryURL}/issues/${goal2}`},
+              ],
+            })
           })
-        }))
-        this.pool1Voters = this.pool1Players.slice()
-
-        await Promise.all(this.pool2Players.map((player, i) => {
-          const [goal1, goal2] = voteDataForPool2.goalNumberVotes[i]
-          return factory.create('vote', {
-            playerId: player.id,
-            poolId: this.pool2.id,
-            goals: [
-              {url: `${this.chapter.goalRepositoryURL}/issues/${goal1}`},
-              {url: `${this.chapter.goalRepositoryURL}/issues/${goal2}`},
-            ],
-          })
-        }))
-        this.pool2Voters = this.pool2Players.slice()
+          this.poolVoters[i] = this.poolPlayers[i].slice()
+        })
       })
 
       const assertValidCycleVotingResults = function (result) {
         const response = result.data.getCycleVotingResults
         expect(response.cycle.id).to.equal(this.cycle.id)
         expect(response.cycle.state).to.equal(this.cycle.state)
-        const responsePool1 = response.pools.find(({name}) => name === this.pool1.name)
-        expect(responsePool1.name).to.equal(this.pool1.name)
-        expect(responsePool1.votingIsStillOpen).to.be.true
-        expect(responsePool1.users.map(_ => _.id).sort(), 'players').to.deep.equal(this.pool1Players.map(_ => _.id).sort())
-        expect(responsePool1.voterPlayerIds.sort(), 'voterPlayerIds').to.deep.equal(this.pool1Voters.map(_ => _.id).sort())
-        expect(responsePool1.candidateGoals[0].goal.url).to.match(new RegExp(`/${voteDataForPool1.firstPlaceGoalNumber}$`))
-        expect(responsePool1.candidateGoals[1].goal.url).to.match(new RegExp(`/${voteDataForPool1.secondPlaceGoalNumber}$`))
-        expect(responsePool1.candidateGoals[2].goal.url).to.match(new RegExp(`/${voteDataForPool1.thirdPlaceGoalNumber}$`))
-        expect(responsePool1.candidateGoals[0].playerGoalRanks.length).to.equal(3)
-        expect(responsePool1.candidateGoals[1].playerGoalRanks.length).to.equal(2)
-        expect(responsePool1.candidateGoals[2].playerGoalRanks.length).to.equal(1)
-
-        const responsePool2 = response.pools.find(({name}) => name === this.pool2.name)
-        expect(responsePool2.name).to.equal(this.pool2.name)
-        expect(responsePool2.votingIsStillOpen).to.be.true
-        expect(responsePool2.users.map(_ => _.id).sort(), 'players').to.deep.equal(this.pool2Players.map(_ => _.id).sort())
-        expect(responsePool2.voterPlayerIds.sort(), 'voterPlayerIds').to.deep.equal(this.pool2Voters.map(_ => _.id).sort())
-        expect(responsePool2.candidateGoals[0].goal.url).to.match(new RegExp(`/${voteDataForPool2.firstPlaceGoalNumber}$`))
-        expect(responsePool2.candidateGoals[1].goal.url).to.match(new RegExp(`/${voteDataForPool2.secondPlaceGoalNumber}$`))
-        expect(responsePool2.candidateGoals[2].goal.url).to.match(new RegExp(`/${voteDataForPool2.thirdPlaceGoalNumber}$`))
-        expect(responsePool2.candidateGoals[0].playerGoalRanks.length).to.equal(3)
-        expect(responsePool2.candidateGoals[1].playerGoalRanks.length).to.equal(2)
-        expect(responsePool2.candidateGoals[2].playerGoalRanks.length).to.equal(1)
+        this.pools.forEach((pool, i) => {
+          const responsePool = response.pools.find(({name}) => name === pool.name)
+          expect(responsePool.name).to.equal(pool.name)
+          expect(responsePool.votingIsStillOpen).to.be.true
+          expect(responsePool.users.map(_ => _.id).sort(), 'players').to.deep.equal(this.poolPlayers[i].map(_ => _.id).sort())
+          expect(responsePool.voterPlayerIds.sort(), 'voterPlayerIds').to.deep.equal(this.poolVoters[i].map(_ => _.id).sort())
+          expect(responsePool.candidateGoals[0].goal.url).to.match(new RegExp(`/${voteDataForPools[i].firstPlaceGoalNumber}$`))
+          expect(responsePool.candidateGoals[1].goal.url).to.match(new RegExp(`/${voteDataForPools[i].secondPlaceGoalNumber}$`))
+          expect(responsePool.candidateGoals[2].goal.url).to.match(new RegExp(`/${voteDataForPools[i].thirdPlaceGoalNumber}$`))
+          expect(responsePool.candidateGoals[0].playerGoalRanks.length).to.equal(3)
+          expect(responsePool.candidateGoals[1].playerGoalRanks.length).to.equal(2)
+          expect(responsePool.candidateGoals[2].playerGoalRanks.length).to.equal(1)
+        })
       }
 
       it('returns results', function () {
@@ -166,7 +144,7 @@ describe(testContext(__filename), function () {
         beforeEach('create an invalid vote', async function () {
           const poolId = this.pools[0].id
           const player = await factory.create('player', {chapterId: this.chapter.id})
-          this.pool1Players.push(player)
+          this.poolPlayers[0].push(player)
           this.players.push(player)
           await addPlayerIdsToPool(poolId, [player.id])
           await factory.create('invalid vote', {playerId: player.id, poolId})
