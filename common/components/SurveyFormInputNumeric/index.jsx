1e187740a9ebc5b0fae2df12010aa18e3b37effc
jeffreywescott <jeffrey@learnersguild.org>
2017-01-20 12:16:44 -0800

    ignore mousewheel events for numeric survey inputs

--- common/components/SurveyFormInputNumeric/index.jsx
+++ common/components/SurveyFormInputNumeric/index.jsx
@@ -5,12 +5,18 @@ class SurveyFormInputNumeric extends React.Component {
   constructor(props) {
     super(props)
     this.handleUpdate = this.handleUpdate.bind(this)
+    this.handleWheel = this.handleWheel.bind(this)
   }
 
   handleUpdate(value) {
     if (this.props.onChange) {
       this.props.onChange(this.props.name, value)
     }
+  }
+
+  handleWheel(e) {
+    // see: https://app.clubhouse.io/learnersguild/story/193/disable-scrolling-to-change-value-in-hours-input-field
+    e.preventDefault()
   }
 
   render() {
@@ -22,6 +28,7 @@ class SurveyFormInputNumeric extends React.Component {
           hint={this.props.hint}
           value={this.props.value || ''}
           onChange={this.handleUpdate}
+          onWheel={this.handleWheel}
           floating
           />
       </section>
