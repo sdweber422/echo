a9f39e548fa582d10853ba2aaf8142822ac360c7
jeffreywescott <jeffrey@learnersguild.org>
2017-01-20 14:35:54 -0800

    helper script to print levels (for validation)

---
+++ scripts/printLevels.js
@@ -0,0 +1,50 @@
+/* eslint-disable import/imports-first */
+
+// FIXME: replace globals with central (non-global) config
+global.__SERVER__ = true
+
+const getPlayerInfo = require('src/server/actions/getPlayerInfo')
+const {findPlayers} = require('src/server/db/player')
+const {mapById} = require('src/server/util')
+const {computePlayerLevel} = require('src/server/util/stats')
+const {finish} = require('./util')
+
+const startedAt = new Date()
+console.log('startedAt:', startedAt)
+run()
+  .then(() => finish(null, {startedAt}))
+  .catch(err => finish(err, {startedAt}))
+
+async function run() {
+  // retrieve all players & user profile info; sort by handle
+  let players = await findPlayers()
+  const playerUsers = await getPlayerInfo(players.map(p => p.id))
+  const playerUserMap = mapById(playerUsers)
+  players = players
+    .map(p => {
+      return {...p, ...(playerUserMap.get(p.id) || {}), level: computePlayerLevel(p)}
+    })
+    .filter(p => p.active && p.roles.indexOf('staff') < 0)
+    .sort((a, b) => a.handle.localeCompare(b.handle))
+
+  printPlayerLevels(players)
+}
+
+async function printPlayerLevels(players) {
+  console.log()
+  players.forEach(player => {
+    console.log(`${player.handle} (${player.name}): ${player.level}`)
+  })
+  console.log()
+  console.log('Level Counts\n============')
+  for (let i = 0; i <= 4; ++i) {
+    const numPlayers = players.reduce((count, player) => {
+      if (player.level === i) {
+        count++
+      }
+      return count
+    }, 0)
+    console.log(`Level ${i}:`, numPlayers)
+  }
+  console.log()
+}
