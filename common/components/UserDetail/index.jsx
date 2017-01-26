be118646b362cca8006df2fedb45c41214873a4d
jeffreywescott <jeffrey@learnersguild.org>
2017-01-25 21:25:48 -0800

    don't render stats if unavailable

--- common/components/UserDetail/index.jsx
+++ common/components/UserDetail/index.jsx
@@ -8,6 +8,7 @@ import UserProjectSummary from 'src/common/components/UserProjectSummary'
 import {Flex} from 'src/common/components/Layout'
 import {formatPartialPhoneNumber} from 'src/common/util/format'
 import {STAT_DESCRIPTORS} from 'src/common/models/stat'
+import {objectValuesAreAllNull} from 'src/common/util'
 
 import styles from './index.scss'
 import theme from './theme.scss'
@@ -26,6 +27,40 @@ class UserDetail extends Component {
     this.setState({tabIndex})
   }
 
+  renderSidebarStatNames(stats) {
+    return !objectValuesAreAllNull(stats) ? (
+      <div>
+        <div>Elo</div>
+        <div>XP</div>
+        <div>Culture</div>
+        <div><nobr>Team Play</nobr></div>
+        <div>Technical</div>
+        <div><nobr>Est. Accy.</nobr></div>
+        <div><nobr>Est. Bias</nobr></div>
+        <div>Challenge</div>
+        <div><nobr># Reviews</nobr></div>
+      </div>
+    ) : <div/>
+  }
+
+  renderSidebarStatValues(stats) {
+    const renderStat = stat => !stat || Number.isFinite(stats[stat]) ? stats[stat] : '--'
+
+    return !objectValuesAreAllNull(stats) ? (
+      <div>
+        <div>{renderStat(STAT_DESCRIPTORS.RATING_ELO)}</div>
+        <div>{renderStat(STAT_DESCRIPTORS.EXPERIENCE_POINTS)}</div>
+        <div>{renderStat(STAT_DESCRIPTORS.CULTURE_CONTRIBUTION)}%</div>
+        <div>{renderStat(STAT_DESCRIPTORS.TEAM_PLAY)}%</div>
+        <div>{renderStat(STAT_DESCRIPTORS.TECHNICAL_HEALTH)}%</div>
+        <div>{renderStat(STAT_DESCRIPTORS.ESTIMATION_ACCURACY)}%</div>
+        <div>{renderStat(STAT_DESCRIPTORS.ESTIMATION_BIAS)}%</div>
+        <div>{renderStat(STAT_DESCRIPTORS.CHALLENGE)}</div>
+        <div>{renderStat(STAT_DESCRIPTORS.NUM_PROJECTS_REVIEWED)}</div>
+      </div>
+    ) : <div/>
+  }
+
   renderSidebar() {
     const {user} = this.props
     const stats = user.stats || {}
@@ -42,8 +77,6 @@ class UserDetail extends Component {
       </a>
     ) : null
 
-    const renderStat = stat => !stat || Number.isFinite(stats[stat]) ? stats[stat] : '--'
-
     return (
       <ContentSidebar
         imageUrl={user.avatarUrl || process.env.LOGO_FULL_URL}
@@ -55,15 +88,7 @@ class UserDetail extends Component {
           <Flex className={styles.section} flexDirection="column">
             <Flex className={styles.list}>
               <Flex className={styles.listLeftCol} flexDirection="column">
-                <div>Elo</div>
-                <div>XP</div>
-                <div>Culture</div>
-                <div><nobr>Team Play</nobr></div>
-                <div>Technical</div>
-                <div><nobr>Est. Accy.</nobr></div>
-                <div><nobr>Est. Bias</nobr></div>
-                <div>Challenge</div>
-                <div><nobr># Reviews</nobr></div>
+                {this.renderSidebarStatNames(stats)}
                 <div><span>&nbsp;</span></div>
                 <div>Email</div>
                 <div>Phone</div>
@@ -73,15 +98,7 @@ class UserDetail extends Component {
                 <div>Updated</div>
               </Flex>
               <Flex className={styles.listRightCol} flexDirection="column">
-                <div>{renderStat(STAT_DESCRIPTORS.RATING_ELO)}</div>
-                <div>{renderStat(STAT_DESCRIPTORS.EXPERIENCE_POINTS)}</div>
-                <div>{renderStat(STAT_DESCRIPTORS.CULTURE_CONTRIBUTION)}%</div>
-                <div>{renderStat(STAT_DESCRIPTORS.TEAM_PLAY)}%</div>
-                <div>{renderStat(STAT_DESCRIPTORS.TECHNICAL_HEALTH)}%</div>
-                <div>{renderStat(STAT_DESCRIPTORS.ESTIMATION_ACCURACY)}%</div>
-                <div>{renderStat(STAT_DESCRIPTORS.ESTIMATION_BIAS)}%</div>
-                <div>{renderStat(STAT_DESCRIPTORS.CHALLENGE)}</div>
-                <div>{renderStat(STAT_DESCRIPTORS.NUM_PROJECTS_REVIEWED)}</div>
+                {this.renderSidebarStatValues(stats)}
                 <div><span>&nbsp;</span></div>
                 <div>{emailLink || '--'}</div>
                 <div>{phoneLink || '--'}</div>
@@ -156,8 +173,15 @@ UserDetail.propTypes = {
       name: PropTypes.string,
     }),
     stats: PropTypes.shape({
-      [STAT_DESCRIPTORS.EXPERIENCE_POINTS]: PropTypes.number,
       [STAT_DESCRIPTORS.RATING_ELO]: PropTypes.number,
+      [STAT_DESCRIPTORS.EXPERIENCE_POINTS]: PropTypes.number,
+      [STAT_DESCRIPTORS.CULTURE_CONTRIBUTION]: PropTypes.number,
+      [STAT_DESCRIPTORS.TEAM_PLAY]: PropTypes.number,
+      [STAT_DESCRIPTORS.TECHNICAL_HEALTH]: PropTypes.number,
+      [STAT_DESCRIPTORS.ESTIMATION_ACCURACY]: PropTypes.number,
+      [STAT_DESCRIPTORS.ESTIMATION_BIAS]: PropTypes.number,
+      [STAT_DESCRIPTORS.CHALLENGE]: PropTypes.number,
+      [STAT_DESCRIPTORS.NUM_PROJECTS_REVIEWED]: PropTypes.number,
     }),
   }),
   userProjectSummaries: PropTypes.array,
