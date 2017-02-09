7f607ebb7ba79c344fd33448d5eac6a27d135183
pllearns@gmail.com <philliplorenzo@Phillips-MacBook-Pro.local>
2017-02-08 16:09:34 -0800

    WIP: Initial search and adding lockedStatus and lockedAndUnlocked to the userCan function and the Project.js to the schema

--- common/util/userCan.js
+++ common/util/userCan.js
@@ -41,6 +41,7 @@ const CAPABILITY_ROLES = {
   getRetrospectiveSurvey: GAME_PLAY,
   findRetrospectiveSurveys: GAME_PLAY,
   getProjectReviewSurveyStatus: GAME_PLAY,
+  lockAndUnlockSurveys: ['moderator', 'player']
 
   runReports: ['moderator', 'sysadmin'],
   monitorJobQueues: ['sysadmin'],
