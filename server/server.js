86b03fad5a5475e94015848f9752604362818aa8
SJ Pratt <pratt.sabra@gmail.com>
2017-02-09 13:48:38 -0500

    addtl minor error handling cleanup

--- server/server.js
+++ server/server.js
@@ -84,17 +84,13 @@ export function start() {
         error: ${config.server.secure ? originalError.toString() : originalError.stack}`)
     }
 
-    res.status(serverError.statusCode)
     if (req.accepts('json')) {
-      const errors = [serverError]
-      if (serverError.originalError) {
-        errors.push(serverError.originalError)
-      }
-      return res.json({errors})
+      return res.json({errors: [serverError]})
     }
 
     const responseBody = `<h1>${serverError.statusCode} - ${serverError.type}</h1><p>${serverError.message}</p>`
-    res.send(responseBody)
+
+    res.status(serverError.statusCode).send(responseBody)
   })
 
   // socket cluster
