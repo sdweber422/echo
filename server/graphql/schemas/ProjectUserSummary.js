edb80e8a5706d7b0cbe265b9708d2b62a79eea69
jeffreywescott <jeffrey@learnersguild.org>
2017-02-22 10:51:48 -0800

    unlock surveys: ensure locked / unlocked status is returned

--- server/graphql/schemas/ProjectUserSummary.js
+++ server/graphql/schemas/ProjectUserSummary.js
@@ -1,4 +1,4 @@
-import {GraphQLNonNull} from 'graphql'
+import {GraphQLNonNull, GraphQLBoolean} from 'graphql'
 import {GraphQLObjectType, GraphQLList} from 'graphql/type'
 
 export default new GraphQLObjectType({
@@ -11,6 +11,8 @@ export default new GraphQLObjectType({
       user: {type: new GraphQLNonNull(UserProfile), description: 'The user'},
       userProjectEvaluations: {type: new GraphQLList(UserProjectEvaluation), description: 'The user\'s project evaluations'},
       userProjectStats: {type: UserProjectStats, description: 'The user\'s project stats'},
+      userRetrospectiveComplete: {type: GraphQLBoolean, description: 'True if the user has completed their retrospective survey for this project'},
+      userRetrospectiveUnlocked: {type: GraphQLBoolean, description: 'True if the user\'s retrospective survey for this project as been completed but is unlocked.'}
     }
   }
 })
