db24e67c7aa560e402d514fd10ffaada54137c28
jeffreywescott <jeffrey@learnersguild.org>
2017-03-02 10:00:24 -0800

    don't lose error messages when API calls fail

--- server/util/api.js
+++ server/util/api.js
@@ -2,13 +2,14 @@ import fetch from 'isomorphic-fetch'
 import parseLinkHeader from 'parse-link-header'
 
 export class APIError extends Error {
-  constructor(status, statusText, url) {
+  constructor(status, statusText, url, errObj = {}) {
     const message = `API Error ${status} (${statusText}) trying to invoke API (url = '${url}')`
     super(message)
     this.name = 'APIError'
     this.status = status
     this.statusText = statusText
     this.url = url
+    this.errObj = errObj
   }
 }
 
@@ -19,30 +20,32 @@ export function apiFetchRaw(url, opts = {}) {
 export function apiFetch(url, opts = {}) {
   return apiFetchRaw(url, opts)
     .then(resp => {
-      if (!resp.ok) {
-        throw new APIError(resp.status, resp.statusText, url)
-      }
-      return resp.json()
+      return resp.json().then(result => {
+        if (!resp.ok) {
+          throw new APIError(resp.status, resp.statusText, url, result)
+        }
+        return result
+      })
     })
 }
 
 export function apiFetchAllPages(url, opts = {}, prevResults = []) {
   return apiFetchRaw(url, opts)
     .then(resp => {
-      if (!resp.ok) {
-        throw new APIError(resp.status, resp.statusText, url)
-      }
-      const link = parseLinkHeader(resp.headers.get('Link'))
-      let next = null
-      if (link && link.next) {
-        next = link.next.results && !eval(link.next.results) ? null : link.next.url // eslint-disable-line no-eval
-      }
-      return resp.json()
-        .then(results => {
-          if (next) {
-            return apiFetchAllPages(next, opts, prevResults.concat(results))
-          }
-          return prevResults.concat(results)
-        })
+      return resp.json().then(results => {
+        if (!resp.ok) {
+          throw new APIError(resp.status, resp.statusText, url, results)
+        }
+
+        const link = parseLinkHeader(resp.headers.get('Link'))
+        let next = null
+        if (link && link.next) {
+          next = link.next.results && !eval(link.next.results) ? null : link.next.url // eslint-disable-line no-eval
+        }
+        if (next) {
+          return apiFetchAllPages(next, opts, prevResults.concat(results))
+        }
+        return prevResults.concat(results)
+      })
     })
 }
