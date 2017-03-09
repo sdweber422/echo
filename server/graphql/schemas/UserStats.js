88904b057372c3471078ae75374c69071be6be52
jeffreywescott <jeffrey@learnersguild.org>
2017-03-09 12:32:17 -0800

    add RXP to UserStats

--- server/graphql/schemas/UserStats.js
+++ server/graphql/schemas/UserStats.js
@@ -20,6 +20,7 @@ export default new GraphQLObjectType({
       [STAT_DESCRIPTORS.EXTERNAL_PROJECT_REVIEW_COUNT]: {type: GraphQLFloat, description: 'Number reviews completed of other players\' projects'},
       [STAT_DESCRIPTORS.INTERNAL_PROJECT_REVIEW_COUNT]: {type: GraphQLFloat, description: 'Number reviews completed by this player of their own projects'},
       [STAT_DESCRIPTORS.PROJECT_REVIEW_ACCURACY]: {type: GraphQLFloat, description: 'Review Accuracy'},
+      [STAT_DESCRIPTORS.PROJECT_REVIEW_EXPERIENCE]: {type: GraphQLFloat, description: 'Review Experience (RXP)'},
     }
   }
 })
