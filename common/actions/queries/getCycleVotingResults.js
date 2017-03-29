1f9d2d20b14d93b6bc9321b4387faf13174618f4
Amelia Clarke <ameliavoncat@gmail.com>
2017-03-29 15:21:00 -0700

    updates the getCycleVotingResults query to reflect mixed level pools

--- common/actions/queries/getCycleVotingResults.js
+++ common/actions/queries/getCycleVotingResults.js
@@ -20,7 +20,7 @@ export default function getCycleVotingResults() {
           pools {
             id
             name
-            level
+            levels
             voterPlayerIds
             users {
               id
