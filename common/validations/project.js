2187299f6ee0e43931ef9007dad5c4bf4da9f711
SJ Pratt <pratt.sabra@gmail.com>
2017-03-30 09:11:33 -0400

    update validation for `coachIdentifier` input in `ProjectForm`

--- common/validations/project.js
+++ common/validations/project.js
@@ -2,21 +2,25 @@ import yup from 'yup'
 
 export const projectSchema = yup.object().shape({
   chapterIdentifier: yup.string().trim().required().min(3),
+  coachIdentifier: yup.string().trim().required().test(
+    'is-valid-user-identifier',
+    'Invalid user identifier',
+    _isValidIdentifier,
+  ),
   cycleIdentifier: yup.number().integer().required().min(1),
   goalIdentifier: yup.number().integer().positive().required(),
   playerIdentifiers: yup.string().trim().required().test(
     'are-valid-user-identifiers',
-    'Invalid user identifiers',
-    isValidIdentifierString,
-  ),
-  coachIdentifier: yup.string().trim().required().test(
-    'is-valid-user-identifier',
-    'Invalid user identifier',
-    isValidIdentifierString,
+    'Invalid user identifier(s)',
+    _isValidIdentifierList,
   ),
 })
 
-function isValidIdentifierString(value) {
+function _isValidIdentifier(value) {
+  return /^[a-z0-9]+$/i.test(value)
+}
+
+function _isValidIdentifierList(value) {
   return (
     (value || '')
       .split(',')
