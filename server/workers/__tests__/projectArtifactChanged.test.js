b132464eadc99f652e132b56746100d5279cf013
jeffreywescott <jeffrey@learnersguild.org>
2017-04-07 16:51:40 -0700

    no project channels anymore -- use MPDMs

--- server/workers/__tests__/projectArtifactChanged.test.js
+++ server/workers/__tests__/projectArtifactChanged.test.js
@@ -3,7 +3,7 @@
 /* eslint-disable prefer-arrow-callback, no-unused-expressions, max-nested-callbacks */
 import stubs from 'src/test/stubs'
 import factory from 'src/test/factories'
-import {withDBCleanup} from 'src/test/helpers'
+import {withDBCleanup, useFixture} from 'src/test/helpers'
 
 describe(testContext(__filename), function () {
   withDBCleanup()
@@ -21,20 +21,24 @@ describe(testContext(__filename), function () {
 
     describe('when a cycle has completed', function () {
       beforeEach(async function () {
+        this.users = await factory.buildMany('user', 4)
+        this.handles = this.users.map(user => user.handle)
         this.project = await factory.create('project', {
           artifactURL: 'https://example.com',
           name: 'curious-cats',
+          playerIds: this.users.map(user => user.id)
         })
+        useFixture.nockIDMGetUsersById(this.users)
       })
 
       it('sends a message to the chapter chatroom', async function () {
         await processProjectArtifactChanged(this.project)
 
-        expect(chatService.sendChannelMessage).to.have.been
-          .calledWithMatch(this.project.name, `[artifact](${this.project.artifactURL})`)
+        expect(chatService.sendMultiPartyDirectMessage).to.have.been
+          .calledWithMatch(this.handles, `[artifact](${this.project.artifactURL})`)
 
-        expect(chatService.sendChannelMessage).to.have.been
-          .calledWithMatch(this.project.name, `#${this.project.name} has been updated`)
+        expect(chatService.sendMultiPartyDirectMessage).to.have.been
+          .calledWithMatch(this.handles, `#${this.project.name} has been updated`)
       })
     })
   })
--- server/workers/projectArtifactChanged.js
+++ server/workers/projectArtifactChanged.js
@@ -1,3 +1,6 @@
+import {mapById} from 'src/common/util'
+import getPlayerInfo from 'src/server/actions/getPlayerInfo'
+
 export function start() {
   const jobService = require('src/server/services/jobService')
   jobService.processJobs('projectArtifactChanged', processProjectArtifactChanged)
@@ -7,7 +10,11 @@ export async function processProjectArtifactChanged(project) {
   const chatService = require('src/server/services/chatService')
 
   console.log(`Project artifact for project #${project.name} changed to ${project.artifactURL}`)
+  const projectUsersById = mapById(
+    await getPlayerInfo(project.playerIds)
+  )
+  const handles = project.playerIds.map(playerId => projectUsersById.get(playerId).handle)
 
-  const announcement = `🔗 * The [artifact](${project.artifactURL}) for #${project.name} has been updated*.`
-  return chatService.sendChannelMessage(project.name, announcement)
+  const announcement = `🔗 * The <${project.artifactURL}|artifact> for #${project.name} has been updated*.`
+  return chatService.sendMultiPartyDirectMessage(handles, announcement)
 }
