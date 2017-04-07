1fa53d416bd8e12f118f37424f016c426995cdc8
Rachel Ralston <rachelralston@gmail.com>
2017-04-06 17:44:06 -0700

    WIP, squash me

---
+++ scripts/migrateChannelsToSlack.js
@@ -0,0 +1,15 @@
+import csvWriter from 'csv-write-stream'
+import fetch from 'isomorphic-fetch'
+
+function fetchTeamChannels() {
+  
+}
+
+// export default function requestHandler(req, res) {
+//   const {chapter} = req.query
+//   const writer = csvWriter()
+//   writer.pipe(res)
+//
+//   return runReport(writer, chapter)
+//     .then(() => writer.end())
+// }
