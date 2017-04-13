bef74b568531532529a1d57ec12fc757af7d8bc6
Trevor Little <trevor@learnersguild.org>
2017-04-13 08:26:01 -0400

    make sure not to use newrelic in dev

--- server/graphql/util/index.js
+++ server/graphql/util/index.js
@@ -1,5 +1,5 @@
 import {GraphQLError} from 'graphql/error'
-import newrelic from 'newrelic'
+import config from 'src/config'
 
 import {parseQueryError} from 'src/server/db/errors'
 
@@ -26,6 +26,11 @@ export function pruneAutoLoad(loadedModules) {
 }
 
 export function instrumentResolvers(fields, prefix) {
+  if (!config.server.newrelic.enabled) {
+    return fields
+  }
+  const newrelic = require('newrelic')
+
   return Object.entries(fields).map(([queryName, schema]) => {
     const originalResolver = schema.resolve
     return {
