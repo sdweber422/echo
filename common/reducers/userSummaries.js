5771110a30531fc3a4b39ecff4c54a284f1b8924
jeffreywescott <jeffrey@learnersguild.org>
2017-04-14 18:03:46 -0700

    ensure that redux state gets properly updated after deactivation

--- common/reducers/userSummaries.js
+++ common/reducers/userSummaries.js
@@ -1,4 +1,7 @@
 import {
+  DEACTIVATE_USER_REQUEST,
+  DEACTIVATE_USER_SUCCESS,
+  DEACTIVATE_USER_FAILURE,
   GET_USER_SUMMARY_REQUEST,
   GET_USER_SUMMARY_SUCCESS,
   GET_USER_SUMMARY_FAILURE,
@@ -28,6 +31,27 @@ export default function userSummaries(state = initialState, action) {
       }
 
     case GET_USER_SUMMARY_FAILURE:
+      return Object.assign({}, state, {
+        isBusy: false,
+      })
+
+    case DEACTIVATE_USER_REQUEST:
+      return Object.assign({}, state, {
+        isBusy: true,
+      })
+    case DEACTIVATE_USER_SUCCESS:
+      {
+        const userAttrs = action.response || {}
+        const userSummary = state.userSummaries[userAttrs.id] || {}
+        const user = Object.assign({}, userSummary.user, userAttrs)
+        const newUserSummary = Object.assign({}, userSummary, {user})
+        const userSummaries = Object.assign({}, state.userSummaries, {[user.id]: newUserSummary})
+        return Object.assign({}, state, {
+          isBusy: false,
+          userSummaries,
+        })
+      }
+    case DEACTIVATE_USER_FAILURE:
       return Object.assign({}, state, {
         isBusy: false,
       })
--- common/reducers/users.js
+++ common/reducers/users.js
@@ -2,9 +2,6 @@ import {
   FIND_USERS_REQUEST,
   FIND_USERS_SUCCESS,
   FIND_USERS_FAILURE,
-  DEACTIVATE_USER_REQUEST,
-  DEACTIVATE_USER_SUCCESS,
-  DEACTIVATE_USER_FAILURE,
 } from 'src/common/actions/types'
 
 import {mergeEntities} from 'src/common/util'
@@ -29,24 +26,6 @@ export default function users(state = initialState, action) {
         })
       }
     case FIND_USERS_FAILURE:
-      return Object.assign({}, state, {
-        isBusy: false,
-      })
-    case DEACTIVATE_USER_REQUEST:
-      return Object.assign({}, state, {
-        isBusy: true,
-      })
-    case DEACTIVATE_USER_SUCCESS:
-      {
-        const user = action.response || {}
-        const users = Object.assign({}, state.users, {[user.id]: user})
-        return Object.assign({}, state, {
-          isBusy: false,
-          users,
-        })
-      }
-
-    case DEACTIVATE_USER_FAILURE:
       return Object.assign({}, state, {
         isBusy: false,
       })
