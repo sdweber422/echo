26886ee4249791c4f05f162967b3cfdefaf1320b
jason00111 <emberleyjason@gmail.com>
2017-05-12 10:00:35 -0700

    add compression package to gzip server responses

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import {HTTPS as https} from 'express-sslify'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import raven from 'raven'
+import compression from 'compression'
 
 import config from 'src/config'
 import configureApp from './configureApp'
@@ -23,6 +24,8 @@ export function start() {
 
   const app = new Express()
   const httpServer = http.createServer(app)
+
+  app.use(compression())
 
   configureApp(app)
 
