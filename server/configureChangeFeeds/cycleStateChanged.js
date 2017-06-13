cf338a745de3800fa8d155346172f147b3552ca8
SJ Pratt <pratt.sabra@gmail.com>
2017-06-13 13:28:40 -0700

    fix casing for changefeed files & imports

--- server/configureChangeFeeds/cycleStateChanged.js
+++ server/configureChangeFeeds/cycleStateChanged.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-console, camelcase */
 import {CYCLE_STATES, PRACTICE} from 'src/common/models/cycle'
-import {changeFeedForCycleStateChanged} from 'src/server/services/dataService'
+import {changefeedForCycleStateChanged} from 'src/server/services/dataService'
 import {processStateChangefeed} from './util'
 
 export default function cycleStateChanged(cycleStateChangedQueues) {
   processStateChangefeed({
-    getFeed: changeFeedForCycleStateChanged,
+    getFeed: changefeedForCycleStateChanged,
     changefeedName: 'cycle state changed',
     states: CYCLE_STATES,
     statesWithExtendedTimeout: [PRACTICE],
--- server/configureChangeFeeds/projectStateChanged.js
+++ server/configureChangeFeeds/projectStateChanged.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-console, camelcase */
 import {PROJECT_STATES, CLOSED, CLOSED_FOR_REVIEW} from 'src/common/models/project'
-import {changeFeedForProjectStateChanged} from 'src/server/services/dataService'
+import {changefeedForProjectStateChanged} from 'src/server/services/dataService'
 import {processStateChangefeed} from './util'
 
 export default function projectStateChanged(projectStateChangedQueues) {
   processStateChangefeed({
-    getFeed: changeFeedForProjectStateChanged,
+    getFeed: changefeedForProjectStateChanged,
     changefeedName: 'project state changed',
     states: PROJECT_STATES,
     outOfOrderStateTransitions: {
