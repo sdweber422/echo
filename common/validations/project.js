05f7252bc76b075259e094b02b405d82ace40fe3
SJ Pratt <pratt.sabra@gmail.com>
2017-06-15 13:12:26 -0700

    remove `coachIdentifier` from project form validation

--- common/validations/project.js
+++ common/validations/project.js
@@ -2,11 +2,6 @@ import yup from 'yup'
 
 export const projectSchema = yup.object().shape({
   chapterIdentifier: yup.string().trim().required().min(3),
-  coachIdentifier: yup.string().trim().test(
-    'is-valid-user-identifier',
-    'Invalid user identifier',
-    _isValidIdentifier,
-  ),
   cycleIdentifier: yup.number().integer().required().min(1),
   goalIdentifier: yup.number().integer().positive().required(),
   playerIdentifiers: yup.string().trim().required().test(
@@ -15,15 +10,6 @@ export const projectSchema = yup.object().shape({
     _isValidIdentifierList,
   ),
 })
-
-function _isValidIdentifier(value) {
-  if (!value) {
-    return true
-  }
-  // https://github.com/shinnn/github-username-regex
-  const githubUsernameRegex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i
-  return githubUsernameRegex.test(value)
-}
 
 function _isValidIdentifierList(value) {
   return (
