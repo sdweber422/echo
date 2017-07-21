eac8fcde8b536bd7c7228511cd8aba278f0c6a6f
SJ Pratt <pratt.sabra@gmail.com>
2017-07-21 12:47:30 -0400

    update ProjectList tests

--- src/common/components/__tests__/ProjectList.test.js
+++ src/common/components/__tests__/ProjectList.test.js
@@ -14,41 +14,55 @@ describe(testContext(__filename), function () {
   const noProjectsMessage = 'No projects found.'
   const tableHeaderName = 'Name'
   const tableHeaderCycle = 'Cycle'
+  const projectModel = {
+    name: {type: String},
+    cycleNumber: {title: 'Cycle', type: String},
+    phaseNumber: {title: 'Phase', type: String},
+    state: {title: 'State', type: String},
+    goalTitle: {title: 'Goal', type: String},
+    hasArtifact: {title: 'Artifact?', type: String},
+    memberHandles: {title: 'Members', type: String},
+  }
 
   function createProjectList(props) {
     return shallow(React.createElement(ProjectList, props))
   }
 
-  function buildProjectProps(projects, cycle) {
-    const projectList = []
+  function buildProjectProps({projects, cycle, phase, users}) {
+    const projectData = []
     projects.forEach(function (project) {
-      const {name} = project
       const {cycleNumber, state} = cycle
-      projectList.push({
-        cycle: {
-          cycleNumber,
-          state,
-        },
-        goal: {
-          title: `Phase ${project.goal.phase}`,
-        },
-        name,
-        phase: {
-          number: project.goal.phase,
-        },
+      projectData.push({
+        cycleNumber,
+        state,
+        name: project.name,
+        phaseNumber: phase.number,
+        goalTitle: project.goal.title,
+        memberHandles: users.map(u => u.handle).join(', '),
       })
     })
-    return projectList
+    return projectData
   }
 
   before(async function () {
     this.cycle = await factory.build('cycle')
-    this.projects = await factory.buildMany('project', {cycleId: this.cycle.id, chapterId: this.cycle.chapterId}, 6)
+    this.phase = await factory.build('phase')
+    this.users = await factory.buildMany('user', 3)
+    this.projects = await factory.buildMany('project', {
+      cycleId: this.cycle.id,
+      chapterId: this.cycle.chapterId,
+      phaseId: this.phase.id,
+      memberIds: this.users.map(u => u.id),
+    }, 6)
     this.getProps = async function (customProps) {
       const baseProps = {
-        allowImport: false,
-        allowSelect: true,
-        projects: buildProjectProps(this.projects, this.cycle)
+        projectModel,
+        projectData: buildProjectProps({
+          cycle: this.cycle,
+          phase: this.phase,
+          projects: this.projects,
+          users: this.users,
+        }),
       }
       return customProps ? Object.assign({}, baseProps, customProps) : baseProps
     }
@@ -60,13 +74,14 @@ describe(testContext(__filename), function () {
       const root = createProjectList(props)
 
       expect(root.html()).to.contain(this.projects[0].name)
+      expect(root.html()).to.contain(this.users.map(u => u.handle).join(', '))
       expect(root.html()).to.contain(this.cycle.state)
       expect(root.html()).to.contain(buttonLabel)
       expect(root.html()).to.contain('<table')
     })
 
     it('should display \'No projects found.\' if no projects exist', async function () {
-      const props = await this.getProps({projects: []})
+      const props = await this.getProps({projectData: []})
       const root = createProjectList(props)
 
       expect(root.html()).to.contain(noProjectsMessage)
